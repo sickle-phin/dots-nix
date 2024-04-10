8358190527b380d470f431aa479a59dd627d93a5
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2024-04-10 21:26:24 +0900

    Add: zram

--- modules/system.nix
+++ modules/system.nix
@@ -226,4 +226,13 @@ in
     udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
   };
   powerManagement.cpuFreqGovernor = "schedutil";
+
+  zramSwap = {
+    enable = true;
+    # one of "lzo", "lz4", "zstd"
+    algorithm = "zstd";
+    priority = 5;
+    memoryPercent = 50;
+  };
+
 }
