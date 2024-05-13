e3e9c2c01e0c5a4baf325dcc4a21cedc7e20c945
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2024-05-14 02:55:45 +0900

    fix: mpv setting

--- home/apps/apps.nix
+++ home/apps/apps.nix
@@ -14,18 +14,20 @@
   programs = {
     mpv = {
       enable = true;
-        scripts = with pkgs.mpvScripts; [
-          uosc # UIの全体的な改善
-          thumbfast # サムネイルの表示
-          mpv-playlistmanager # Shift + Enterでプレイリストを表示
-          pkgs.mpvScripts.mpris
-        ];
+      defaultProfiles = ["gpu-hq"];
+      scripts = with pkgs.mpvScripts; [
+        uosc # UIの全体的な改善
+        thumbfast # サムネイルの表示
+        mpv-playlistmanager # Shift + Enterでプレイリストを表示
+        mpris
+      ];
       scriptOpts = {
         # thumbfast.network = "yes"; # YouTubeのサムネイルを表示する設定だが実用性はイマイチ
         # playlistmanager.key_showplaylist = "F8"; # プレイリスト表示のショートカットキーを変更したい場合
       };
       config = {
-        hwdec = "auto"; # HW対応、デフォルト値は"no"
+        hwdec = "no"; # HW対応、デフォルト値は"no"
+        vo = "gpu";
         loop-playlist="inf"; # 再生終了後に最初から再生し直す(プレイリスト単位)
 
         # mpvでYouTubeの視聴をしたい場合はyoutube-dlが必要だが
