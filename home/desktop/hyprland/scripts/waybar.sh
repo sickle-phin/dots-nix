f55d29d619f557b694aa574d1e7e9ff8ef39869e
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2024-05-27 21:38:51 +0900

    Fix: waybar script

--- home/desktop/hyprland/scripts/waybar.sh
+++ home/desktop/hyprland/scripts/waybar.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ "$HOST" = "pink" ]]; then
+if [[ "$HOST" -eq "pink" ]]; then
     pidof waybar || waybar -c ~/.config/waybar/config_laptop
 else
     pidof waybar || waybar -c ~/.config/waybar/config_desktop
