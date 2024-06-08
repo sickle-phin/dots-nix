cccc8fbca4cc85d833e18efa4ef9776c0c79c738
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2024-06-09 02:50:16 +0900

    Add: setting for plymouth

--- hosts/irukaha/default.nix
+++ hosts/irukaha/default.nix
@@ -33,6 +33,10 @@
     };
     kernelPackages = pkgs.linuxPackages_xanmod_latest;
     plymouth.enable = true; 
+    plymouth.theme = "breeze";
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    initrd.systemd.network.wait-online.enable = false;
     tmp.cleanOnBoot = true;
   };
 
