7c6971268b1d2213cddb89bdb7623a9254872b7b
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2024-06-25 02:20:26 +0900

    pipewire: change latency lower

--- modules/sound.nix
+++ modules/sound.nix
@@ -10,6 +10,10 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
-    lowLatency.enable = true;
+    lowLatency = {
+      enable = true;
+      quantum = 32;
+      rate = 96000;
+    };
   };
 }
