a6420b71200bbe4d4ed928a4995c6165962e8364
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2024-06-28 18:56:27 +0900

    sddm: change settings

--- modules/gui.nix
+++ modules/gui.nix
@@ -1,31 +1,27 @@
-{ config
-, pkgs
-, lib
+{ pkgs
 , ...
-}:{
+}: {
   services = {
     xserver = {
       excludePackages = [ pkgs.xterm ];
-      displayManager.setupCommands = "xrandr --output DP-1 --auto --primary xrandr --output HDMI-A-1 --left-of DP-1 --noprimary";
     };
 
     displayManager = {
       sddm = {
         enable = true;
-        wayland.enable = lib.mkIf (config.networking.hostName != "irukaha") true;
+        wayland.enable = true;
         wayland.compositor = "kwin";
         enableHidpi = true;
         theme = "chili";
         settings.Theme = {
-          FacesDir = "/var/lib/AccountsService/icons";
+          FacesDir = "/etc/var/lib/sddm/icons";
           CursorTheme = "breeze_cursors";
         };
       };
     };
   };
 
-  system.activationScripts.script.text = ''
-    mkdir -p /var/lib/AccountsService/icons
-    cp /home/sickle-phin/dots-nix/modules/sickle-phin.face.icon /var/lib/AccountsService/icons
-  '';
+  environment.etc = {
+    "var/lib/sddm/icons/sickle-phin.face.icon".source = /home/sickle-phin/dots-nix/modules/sickle-phin.face.icon;
+  };
 }
