c8e4a426445835c4f5d1c164316eba60e17ee50a
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2024-06-30 01:55:31 +0900

    upgrade stateVersion

--- hosts/irukaha/default.nix
+++ hosts/irukaha/default.nix
@@ -99,7 +99,7 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  system.stateVersion = "23.11";
+  system.stateVersion = "24.05";
 
 }
 
