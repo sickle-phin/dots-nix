c9076d35481e060751d38ed85344589b061c6b05
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2024-07-08 22:56:34 +0900

    Add: microcode update

--- home/desktop/waybar/default.nix
+++ home/desktop/waybar/default.nix
@@ -86,10 +86,10 @@
             warning = 30;
             critical = 15;
           };
-          format = "{icon}   {capacity}% ";
-          format-charging = "   {capacity}% ";
-          format-plugged = "   {capacity}% ";
-          format-alt = "{icon}   {time} ";
+          format = "{icon} {capacity}%";
+          format-charging = " {capacity}%";
+          format-plugged = "  {capacity}%";
+          format-alt = "{icon} {time}";
           # format-good = "";
           # format-full = "";
           format-icons = [ "" "" "" "" "" ];
