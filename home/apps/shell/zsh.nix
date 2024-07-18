b6f3858fd259061f16d7334fde063d2b3fb561df
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2024-07-19 03:11:51 +0900

    change the dolphin icon

--- home/apps/shell/zsh.nix
+++ home/apps/shell/zsh.nix
@@ -52,7 +52,7 @@
       if [[ "$TERM" == *"wezterm"* ]]; then
           fastfetch --iterm ${../../../system/sickle-phin.face.icon} --logo-width 10 --logo-height 5 --logo-padding-top 1
       elif [[ "$TERM" == *"foot"* ]]; then
-          fastfetch --sixel ${./sickle.sixel} --logo-width 11 --logo-height 5 --logo-padding-top 1
+          fastfetch --raw ${./sickle.sixel} --logo-width 10 --logo-height 6 --logo-padding-top 1
       else
           fastfetch
       fi
