c2171461f879fe51de7a24e5540242363cb415f2
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2024-07-20 00:44:52 +0900

    fastfetch: custom

--- home/apps/fastfetch.nix
+++ home/apps/fastfetch.nix
@@ -5,22 +5,71 @@
       modules = [
         "title"
         "separator"
-        "os"
-        "kernel"
-        "uptime"
-        "shell"
-        "display"
-        "de"
-        "wm"
-        "wmtheme"
-        "terminal"
-        "cpu"
-        "gpu"
-        "memory"
-        "swap"
-        "disk"
+        {
+          type = "os";
+          key = "╭─  ";
+          keyColor = "cyan";
+        }
+        {
+          type = "kernel";
+          key = "├─  ";
+          keyColor = "cyan";
+        }
+        {
+          type = "uptime";
+          key = "╰─ 󰅐 ";
+          keyColor = "cyan";
+        }
+        {
+          type = "display";
+          key = "╭─ 󰍹 ";
+          keyColor = "magenta";
+        }
+        {
+          type = "cpu";
+          key = "├─ 󰻠 ";
+          keyColor = "magenta";
+        }
+        {
+          type = "gpu";
+          key = "├─ 󰍛 ";
+          keyColor = "magenta";
+        }
+        {
+          type = "memory";
+          key = "├─ 󰑭 ";
+          keyColor = "magenta";
+        }
+        {
+          type = "disk";
+          key = "├─  ";
+          keyColor = "magenta";
+        }
+        {
+          type = "swap";
+          key = "╰─ 󰓡 ";
+          keyColor = "magenta";
+        }
+        {
+          type = "wm";
+          key = "╭─  ";
+          keyColor = "yellow";
+        }
+        {
+          type = "terminal";
+          key = "├─  ";
+          keyColor = "yellow";
+        }
+        {
+          type = "shell";
+          key = "╰─  ";
+          keyColor = "yellow";
+        }
         "break"
-        "colors"
+        {
+          type = "custom";
+          format = " {#90}  {#31}  {#32}  {#33}  {#34}  {#35}  {#36}  {#37}";
+        }
       ];
     };
   };
--- home/apps/shell/zsh.nix
+++ home/apps/shell/zsh.nix
@@ -52,7 +52,7 @@
       if [[ "$TERM" == *"wezterm"* ]]; then
           fastfetch --iterm ${../../../system/sickle-phin.face.icon} --logo-width 10 --logo-height 5 --logo-padding-top 1
       elif [[ "$TERM" == *"foot"* ]]; then
-          fastfetch --raw ${./sickle.sixel} --logo-width 10 --logo-height 6 --logo-padding-top 1
+          fastfetch --raw ${./sickle.sixel} --logo-width 9 --logo-height 6 --logo-padding-top 1 --logo-padding-left 1
       else
           fastfetch
       fi
