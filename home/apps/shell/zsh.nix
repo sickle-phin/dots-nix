42415f6522d2c32fcb7e1e23d242890abd34392e
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2024-08-11 04:57:29 +0900

    zsh: add foot config

--- home/apps/shell/zsh.nix
+++ home/apps/shell/zsh.nix
@@ -44,19 +44,29 @@
     initExtra = ''
       bindkey "^[[3~" delete-char
 
+      function osc7-pwd() {
+        emulate -L zsh # also sets localoptions for us
+        setopt extendedglob
+        local LC_ALL=C
+        printf '\e]7;file://%s%s\e\' $HOST ''${PWD//(#m)([^@-Za-z&-;_~])/%''${(l:2::0:)$(([##16]#MATCH))}}
+      }
+
+      function chpwd-osc7-pwd() {
+        (( ZSH_SUBSHELL )) || osc7-pwd
+      }
+      add-zsh-hook -Uz chpwd chpwd-osc7-pwd
+
       export FZF_DEFAULT_OPTS=" \
         --color=bg+:#313244,spinner:#f5e0dc,hl:#f38ba8 \
         --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
         --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
-      if [[ "$TERM" == *"wezterm"* ]]; then
-          fastfetch --iterm ${../../../system/sickle-phin.face.icon} --logo-width 10 --logo-height 5 --logo-padding-top 1
-      elif [[ "$TERM" == *"foot"* && "$HOST" == *"labo"* ]]; then
-          fastfetch --raw ${./sickle.sixel} --logo-width 5 --logo-height 5 --logo-padding-top 1 --logo-padding-left 1
+      if [[ "$TERM" == *"foot"* && "$HOST" == *"labo"* ]]; then
+        fastfetch --raw ${./sickle.sixel} --logo-width 5 --logo-height 5 --logo-padding-top 1 --logo-padding-left 1
       elif [[ "$TERM" == *"foot"* ]]; then
-          fastfetch --raw ${./sickle.sixel} --logo-width 9 --logo-height 6 --logo-padding-top 1 --logo-padding-left 1
+        fastfetch --raw ${./sickle.sixel} --logo-width 9 --logo-height 6 --logo-padding-top 1 --logo-padding-left 1
       else
-          fastfetch
+        fastfetch
       fi
     '';
   };
