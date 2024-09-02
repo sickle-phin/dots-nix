535c999d46542373d022e29c535c2a497e87ace4
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2024-09-02 16:55:29 +0900

    labo: add gpg key

--- hosts/labo/default.nix
+++ hosts/labo/default.nix
@@ -15,6 +15,7 @@
       "HDMI-A-1,3840x2160@60,0x0,1.5,bitdepth,10,vrr,1"
       ", preferred, auto, 1"
     ];
+    signingKey = "4FA80D9860EC2448";
   };
 
   time.hardwareClockInLocalTime = true;
