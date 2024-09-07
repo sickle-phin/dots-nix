e2ffed69511d55a8c9346d69687fd23c92cfc588
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2024-09-07 22:29:12 +0900

    firefox: add settings

--- home/apps/browsers.nix
+++ home/apps/browsers.nix
@@ -5,7 +5,6 @@
       enable = true;
       commandLineArgs = [
         "--enable-wayland-ime"
-        "--gtk-version=4"
       ];
     };
 
@@ -16,7 +15,9 @@
         name = "default";
         isDefault = true;
         settings = {
+          # reference: https://github.com/arkenfox/user.js/blob/master/user.js
           "app.update.auto" = false;
+
           "browser.aboutConfig.showWarning" = false;
           "browser.startup.page" = 3; # Restore previous session
           "browser.startup.homepage" = "about:blank";
@@ -24,11 +25,14 @@
           "browser.newtabpage.activity-stream.showSponsored" = false;
           "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
           "browser.newtabpage.activity-stream.default.sites" = "";
+
           "browser.provider.use_geoclue" = false;
+
           "extensions.getAddons.showPane" = false;
           "extensions.htmlaboutaddons.recommendations.enabled" = false;
           "browser.discovery.enabled" = false;
           "browser.shopping.experience2023.enabled" = false;
+
           "datareporting.policy.dataSubmissionEnabled" = false;
           "datareporting.healthreport.uploadEnabled" = false;
           "toolkit.telemetry.unified" = false;
@@ -54,7 +58,42 @@
           "captivedetect.canonicalURL" = "";
           "network.captive-portal-service.enabled" = false;
           "network.connectivity-service.enabled" = false;
+
           "browser.safebrowsing.downloads.remote.enabled" = false;
+
+          "network.prefetch-next" = false;
+          "network.dns.disablePrefetch" = true;
+          "network.dns.disablePrefetchFromHTTPS" = true;
+          "network.predictor.enabled" = false;
+          "network.predictor.enable-prefetch" = false;
+          "network.http.speculative-parallel-limit" = 0;
+          "browser.places.speculativeConnect.enabled" = false;
+
+          "browser.urlbar.speculativeConnect.enabled" = false;
+          "browser.urlbar.quicksuggest.enabled" = false;
+          "browser.urlbar.suggest.quicksuggest.nonsponsored" = false;
+          "browser.urlbar.suggest.quicksuggest.sponsored" = false;
+          "browser.search.suggest.enabled" = false;
+          "browser.urlbar.suggest.searches" = false;
+          "browser.urlbar.trending.featureGate" = false;
+          "browser.urlbar.addons.featureGate" = false;
+          "browser.urlbar.mdn.featureGate" = false;
+          "browser.urlbar.pocket.featureGate" = false;
+          "browser.urlbar.weather.featureGate" = false;
+          "browser.urlbar.yelp.featureGate" = false;
+          "browser.formfill.enable" = false;
+          "browser.search.separatePrivateDefault" = true;
+          "browser.search.separatePrivateDefault.ui.enabled" = true;
+
+          "signon.autofillForms" = false;
+          "signon.formlessCapture.enabled" = false;
+          "network.auth.subresource-http-auth-allow" = 1;
+
+          "browser.cache.disk.enable" = false;
+          "browser.privatebrowsing.forceMediaMemoryCache" = true;
+          "media.memory_cache_max_size" = 65536;
+          "browser.sessionstore.privacy_level" = 2;
+
           "browser.warnOnQuit" = false;
           "browser.quitShortcut.disabled" = if pkgs.stdenv.isLinux then true else false;
           "browser.tabs.allow_transparent_browser" = true;
