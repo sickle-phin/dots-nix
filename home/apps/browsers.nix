cbebe1d7e5db22f86a359b835e5d9d6d54ac197a
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2024-09-09 00:59:55 +0900

    firefox: add smoothfox settings

--- home/apps/browsers.nix
+++ home/apps/browsers.nix
@@ -129,6 +129,20 @@ with lib;
           "gfx.webrender.super-resolution.nvidia" = mkIf (osConfig.myOptions.gpu == "nvidia") true;
           "gfx.webrender.overlay-vp-auto-hdr" = mkIf (osConfig.myOptions.gpu == "nvidia") true;
           "gfx.webrender.overlay-vp-super-resolution" = mkIf (osConfig.myOptions.gpu == "nvidia") true;
+
+          # reference https://github.com/yokoffing/Betterfox
+          "apz.overscroll.enabled" = true;
+          "general.smoothScroll" = true;
+          "general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS" = 12;
+          "general.smoothScroll.msdPhysics.enabled" = true;
+          "general.smoothScroll.msdPhysics.motionBeginSpringConstant" = 600;
+          "general.smoothScroll.msdPhysics.regularSpringConstant" = 650;
+          "general.smoothScroll.msdPhysics.slowdownMinDeltaMS" = 25;
+          "general.smoothScroll.msdPhysics.slowdownMinDeltaRatio" = "2";
+          "general.smoothScroll.msdPhysics.slowdownSpringConstant" = 250;
+          "general.smoothScroll.currentVelocityWeighting" = "1";
+          "general.smoothScroll.stopDecelerationWeighting" = "1";
+          "mousewheel.default.delta_multiplier_y" = 250;
         };
         userChrome = ''
           :root {
