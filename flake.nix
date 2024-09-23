7f5d78551685bce81e7daba77881ea27c58463fb
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2024-09-23 23:53:50 +0900

    fcitx5: fix catppuccin theme

--- flake.nix
+++ flake.nix
@@ -54,7 +54,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    catppuccin.url = "github:catppuccin/nix";
+    catppuccin.url = "github:catppuccin/nix/45745fe5960acaefef2b60f3455bcac6a0ca6bc9";
 
     xremap-flake.url = "github:xremap/nix-flake";
   };
