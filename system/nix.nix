c8b7c1de7155e74c76ad759c28b5209afaa7202d
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2024-10-05 21:19:05 +0900

    nix: add setting

--- system/nix.nix
+++ system/nix.nix
@@ -9,6 +9,7 @@
         "nix-command"
         "flakes"
       ];
+      http-connections = 50;
       substituters = [ "https://cache.nixos.org" ];
       trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
       trusted-users = [
