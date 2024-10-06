0c5d76b6b17c4d3ee38f77cc1529bb61ac57f382
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2024-10-07 04:56:58 +0900

    add systemd settings

--- system/security/systemd.nix
+++ system/security/systemd.nix
@@ -71,6 +71,294 @@
       };
     };
 
+    display-manager = {
+      serviceConfig = {
+        ProtectKernelTunables = true;
+        ProtectKernelModules = true;
+        ProtectKernelLogs = true; # so we won't need all of this
+      };
+    };
+
+    emergency = {
+      serviceConfig = {
+        ProtectSystem = "strict";
+        ProtectHome = true;
+        ProtectKernelTunables = true;
+        ProtectKernelModules = true;
+        ProtectControlGroups = true;
+        ProtectKernelLogs = true;
+        ProtectClock = true;
+        ProtectProc = "invisible";
+        ProcSubset = "pid";
+        PrivateTmp = true;
+        PrivateUsers = true;
+        PrivateDevices = true; # Might need adjustment for emergency access
+        PrivateIPC = true;
+        MemoryDenyWriteExecute = true;
+        NoNewPrivileges = true;
+        LockPersonality = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+        RestrictAddressFamilies = "AF_INET";
+        RestrictNamespaces = true;
+        SystemCallFilter = [
+          "write"
+          "read"
+          "openat"
+          "close"
+          "brk"
+          "fstat"
+          "lseek"
+          "mmap"
+          "mprotect"
+          "munmap"
+          "rt_sigaction"
+          "rt_sigprocmask"
+          "ioctl"
+          "nanosleep"
+          "select"
+          "access"
+          "execve"
+          "getuid"
+          "arch_prctl"
+          "set_tid_address"
+          "set_robust_list"
+          "prlimit64"
+          "pread64"
+          "getrandom"
+        ];
+        UMask = "0077";
+        IPAddressDeny = "any";
+      };
+    };
+
+    "getty@tty1" = {
+      serviceConfig = {
+        ProtectSystem = "strict";
+        ProtectHome = true;
+        ProtectKernelTunables = true;
+        ProtectKernelModules = true;
+        ProtectControlGroups = true;
+        ProtectKernelLogs = true;
+        ProtectClock = true;
+        ProtectProc = "invisible";
+        ProcSubset = "pid";
+        PrivateTmp = true;
+        PrivateUsers = true;
+        PrivateDevices = true;
+        PrivateIPC = true;
+        MemoryDenyWriteExecute = true;
+        NoNewPrivileges = true;
+        LockPersonality = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+        RestrictAddressFamilies = "AF_INET";
+        RestrictNamespaces = true;
+        SystemCallFilter = [
+          "write"
+          "read"
+          "openat"
+          "close"
+          "brk"
+          "fstat"
+          "lseek"
+          "mmap"
+          "mprotect"
+          "munmap"
+          "rt_sigaction"
+          "rt_sigprocmask"
+          "ioctl"
+          "nanosleep"
+          "select"
+          "access"
+          "execve"
+          "getuid"
+          "arch_prctl"
+          "set_tid_address"
+          "set_robust_list"
+          "prlimit64"
+          "pread64"
+          "getrandom"
+        ];
+        SystemCallArchitectures = "native";
+        UMask = "0077";
+        IPAddressDeny = "any";
+      };
+    };
+
+    "getty@tty7" = {
+      serviceConfig = {
+        ProtectSystem = "strict";
+        ProtectHome = true;
+        ProtectKernelTunables = true;
+        ProtectKernelModules = true;
+        ProtectControlGroups = true;
+        ProtectKernelLogs = true;
+        ProtectClock = true;
+        ProtectProc = "invisible";
+        ProcSubset = "pid";
+        PrivateTmp = true;
+        PrivateUsers = true;
+        PrivateDevices = true;
+        PrivateIPC = true;
+        MemoryDenyWriteExecute = true;
+        NoNewPrivileges = true;
+        LockPersonality = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+        RestrictAddressFamilies = "AF_INET";
+        RestrictNamespaces = true;
+        SystemCallFilter = [
+          "write"
+          "read"
+          "openat"
+          "close"
+          "brk"
+          "fstat"
+          "lseek"
+          "mmap"
+          "mprotect"
+          "munmap"
+          "rt_sigaction"
+          "rt_sigprocmask"
+          "ioctl"
+          "nanosleep"
+          "select"
+          "access"
+          "execve"
+          "getuid"
+          "arch_prctl"
+          "set_tid_address"
+          "set_robust_list"
+          "prlimit64"
+          "pread64"
+          "getrandom"
+        ];
+        SystemCallArchitectures = "native";
+        UMask = "0077";
+        IPAddressDeny = "any";
+      };
+    };
+
+    nix-daemon = {
+      serviceConfig = {
+        ProtectHome = true;
+        PrivateUsers = false;
+      };
+    };
+
+    rescue = {
+      serviceConfig = {
+        ProtectSystem = "strict";
+        ProtectHome = true;
+        ProtectKernelTunables = true;
+        ProtectKernelModules = true;
+        ProtectControlGroups = true;
+        ProtectKernelLogs = true;
+        ProtectClock = true;
+        ProtectProc = "invisible";
+        ProcSubset = "pid";
+        PrivateTmp = true;
+        PrivateUsers = true;
+        PrivateDevices = true; # Might need adjustment for rescue operations
+        PrivateIPC = true;
+        MemoryDenyWriteExecute = true;
+        NoNewPrivileges = true;
+        LockPersonality = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+        RestrictAddressFamilies = "AF_INET AF_INET6"; # Networking might be necessary in rescue mode
+        RestrictNamespaces = true;
+        SystemCallFilter = [
+          "write"
+          "read"
+          "openat"
+          "close"
+          "brk"
+          "fstat"
+          "lseek"
+          "mmap"
+          "mprotect"
+          "munmap"
+          "rt_sigaction"
+          "rt_sigprocmask"
+          "ioctl"
+          "nanosleep"
+          "select"
+          "access"
+          "execve"
+          "getuid"
+          "arch_prctl"
+          "set_tid_address"
+          "set_robust_list"
+          "prlimit64"
+          "pread64"
+          "getrandom"
+        ];
+        SystemCallArchitectures = "native";
+        UMask = "0077";
+        IPAddressDeny = "any"; # May need to be relaxed for network troubleshooting in rescue mode
+      };
+    };
+
+    "systemd-ask-password-console" = {
+      serviceConfig = {
+        ProtectSystem = "strict";
+        ProtectHome = true;
+        ProtectKernelTunables = true;
+        ProtectKernelModules = true;
+        ProtectControlGroups = true;
+        ProtectKernelLogs = true;
+        ProtectClock = true;
+        ProtectProc = "invisible";
+        ProcSubset = "pid";
+        PrivateTmp = true;
+        PrivateUsers = true;
+        PrivateDevices = true; # May need adjustment for console access
+        PrivateIPC = true;
+        MemoryDenyWriteExecute = true;
+        NoNewPrivileges = true;
+        LockPersonality = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+        RestrictAddressFamilies = "AF_INET AF_INET6";
+        RestrictNamespaces = true;
+        SystemCallFilter = [ "@system-service" ]; # A more permissive filter
+        SystemCallArchitectures = "native";
+        UMask = "0077";
+        IPAddressDeny = "any";
+      };
+    };
+
+    "systemd-ask-password-wall" = {
+      serviceConfig = {
+        ProtectSystem = "strict";
+        ProtectHome = true;
+        ProtectKernelTunables = true;
+        ProtectKernelModules = true;
+        ProtectControlGroups = true;
+        ProtectKernelLogs = true;
+        ProtectClock = true;
+        ProtectProc = "invisible";
+        ProcSubset = "pid";
+        PrivateTmp = true;
+        PrivateUsers = true;
+        PrivateDevices = true;
+        PrivateIPC = true;
+        MemoryDenyWriteExecute = true;
+        NoNewPrivileges = true;
+        LockPersonality = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+        RestrictAddressFamilies = "AF_INET AF_INET6";
+        RestrictNamespaces = true;
+        SystemCallFilter = [ "@system-service" ]; # A more permissive filter
+        SystemCallArchitectures = "native";
+        UMask = "0077";
+        IPAddressDeny = "any";
+      };
+    };
+
     systemd-journald = {
       serviceConfig = {
         UMask = 77;
@@ -108,6 +396,35 @@
         IPAddressDeny = "any";
         DeviceAllow = [ ];
         RestrictAddressFamilies = [ ];
+      };
+    };
+
+    "user@1000" = {
+      serviceConfig = {
+        ProtectSystem = "strict";
+        ProtectHome = true;
+        ProtectKernelTunables = true;
+        ProtectKernelModules = true;
+        ProtectControlGroups = true;
+        ProtectKernelLogs = true;
+        ProtectClock = true;
+        ProtectProc = "invisible";
+        ProcSubset = "pid";
+        PrivateTmp = true;
+        PrivateUsers = true; # Be cautious, as this may restrict user operations
+        PrivateDevices = true;
+        PrivateIPC = true;
+        MemoryDenyWriteExecute = true;
+        NoNewPrivileges = true;
+        LockPersonality = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+        RestrictAddressFamilies = "AF_INET AF_INET6";
+        RestrictNamespaces = true;
+        SystemCallFilter = [ "@system-service" ]; # Adjust based on user needs
+        SystemCallArchitectures = "native";
+        UMask = "0077";
+        IPAddressDeny = "any";
       };
     };
   };
