c9f21ed231426c1215faf3f1cc0cbf8ce67eb40c
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2024-10-13 18:18:52 +0900

    usbguard: edit setting

--- hosts/irukaha/default.nix
+++ hosts/irukaha/default.nix
@@ -26,13 +26,13 @@
     usbguard = {
       enable = true;
       rules = ''
-        allow id 056e:00e4 serial "" name "ELECOM BlueLED Mouse" hash "RIglSjpBvzvt6hj8DgwXrivAD4mu/tw/iXnpVuUFYPc=" parent-hash "jEP/6WzviqdJ5VSeTUY8PatCNBKeaREvo2OqdplND/o=" via-port "1-10" with-interface { 03:01:02 03:00:00 03:00:00 } with-connect-type "hotplug"
-        allow id 3554:fa09 serial "" name "2.4G Wireless Receiver" hash "qrdhK54TwFaT/u6ozq2EsmXzLxu3sWNfFWtzuUdswpY=" parent-hash "+XquLJ1PcxL46fLrSbVNwpIn3oJQYx8VHJUWeqlOsws=" via-port "1-3" with-interface { 03:01:01 03:01:02 } with-connect-type "hotplug"
-        allow id 1ea7:0066 serial "" name "2.4G Mouse" hash "YjVmEGYRcwJqbI/ndHVSSGE84WM6ppRrsXsRMRvZeQQ=" parent-hash "+XquLJ1PcxL46fLrSbVNwpIn3oJQYx8VHJUWeqlOsws=" via-port "1-4" with-interface { 03:01:01 03:01:02 } with-connect-type "hotplug"
-        allow id 05e3:0610 serial "" name "USB2.0 Hub" hash "p0xurg8ayXbgrHAmFBL5BElzY8M/rvDA/w3li6d/wtw=" parent-hash "+XquLJ1PcxL46fLrSbVNwpIn3oJQYx8VHJUWeqlOsws=" via-port "1-7" with-interface { 09:00:01 09:00:02 } with-connect-type "hotplug"
-        allow id 0e8d:0608 serial "000000000" name "Wireless_Device" hash "lcL25SKcS6sKWfdRl/KurZZ/6q9takvgKyf+UFpaWBk=" parent-hash "+XquLJ1PcxL46fLrSbVNwpIn3oJQYx8VHJUWeqlOsws=" via-port "1-8" with-interface { e0:01:01 e0:01:01 e0:01:01 e0:01:01 e0:01:01 e0:01:01 e0:01:01 e0:01:01 e0:01:01 e0:01:01 } with-connect-type "hotplug"
-        allow id 26ce:01a2 serial "A02019100900" name "LED Controller" hash "WdEIqOrWT1p6UWeWoigz6TB5lGjHJbCt5NwfiEgXKw0=" parent-hash "+Lsm3uXJrL0KwWr8E3Phv/ov65s/QLIBmiqongfUTzc=" via-port "7-1" with-interface 03:00:00 with-connect-type "hotplug"
-        allow id 057e:2009 serial "000000000001" name "Pro Controller" hash "+157t1YB+XgpiViQkQNzRvNV8ZJcI0LWaB6GYpHRjhQ=" parent-hash "jEP/6WzviqdJ5VSeTUY8PatCNBKeaREvo2OqdplND/o=" via-port "1-3" with-interface 03:00:00 with-connect-type "hotplug"
+        allow id 056e:00e4 serial "" name "ELECOM BlueLED Mouse" hash "RIglSjpBvzvt6hj8DgwXrivAD4mu/tw/iXnpVuUFYPc=" parent-hash "jEP/6WzviqdJ5VSeTUY8PatCNBKeaREvo2OqdplND/o=" with-interface { 03:01:02 03:00:00 03:00:00 } with-connect-type "hotplug"
+        allow id 3554:fa09 serial "" name "2.4G Wireless Receiver" hash "qrdhK54TwFaT/u6ozq2EsmXzLxu3sWNfFWtzuUdswpY=" parent-hash "+XquLJ1PcxL46fLrSbVNwpIn3oJQYx8VHJUWeqlOsws=" with-interface { 03:01:01 03:01:02 } with-connect-type "hotplug"
+        allow id 1ea7:0066 serial "" name "2.4G Mouse" hash "YjVmEGYRcwJqbI/ndHVSSGE84WM6ppRrsXsRMRvZeQQ=" parent-hash "+XquLJ1PcxL46fLrSbVNwpIn3oJQYx8VHJUWeqlOsws=" with-interface { 03:01:01 03:01:02 } with-connect-type "hotplug"
+        allow id 05e3:0610 serial "" name "USB2.0 Hub" hash "p0xurg8ayXbgrHAmFBL5BElzY8M/rvDA/w3li6d/wtw=" parent-hash "+XquLJ1PcxL46fLrSbVNwpIn3oJQYx8VHJUWeqlOsws=" with-interface { 09:00:01 09:00:02 } with-connect-type "hotplug"
+        allow id 0e8d:0608 serial "000000000" name "Wireless_Device" hash "lcL25SKcS6sKWfdRl/KurZZ/6q9takvgKyf+UFpaWBk=" parent-hash "+XquLJ1PcxL46fLrSbVNwpIn3oJQYx8VHJUWeqlOsws=" with-interface { e0:01:01 e0:01:01 e0:01:01 e0:01:01 e0:01:01 e0:01:01 e0:01:01 e0:01:01 e0:01:01 e0:01:01 } with-connect-type "hotplug"
+        allow id 26ce:01a2 serial "A02019100900" name "LED Controller" hash "WdEIqOrWT1p6UWeWoigz6TB5lGjHJbCt5NwfiEgXKw0=" parent-hash "+Lsm3uXJrL0KwWr8E3Phv/ov65s/QLIBmiqongfUTzc=" with-interface 03:00:00 with-connect-type "hotplug"
+        allow id 057e:2009 serial "000000000001" name "Pro Controller" hash "+157t1YB+XgpiViQkQNzRvNV8ZJcI0LWaB6GYpHRjhQ=" parent-hash "jEP/6WzviqdJ5VSeTUY8PatCNBKeaREvo2OqdplND/o=" with-interface 03:00:00 with-connect-type "hotplug"
       '';
     };
   };
--- hosts/labo/default.nix
+++ hosts/labo/default.nix
@@ -26,11 +26,11 @@
     usbguard = {
       enable = true;
       rules = ''
-        allow id 056e:00e4 serial "" name "ELECOM BlueLED Mouse" hash "RIglSjpBvzvt6hj8DgwXrivAD4mu/tw/iXnpVuUFYPc=" parent-hash "jEP/6WzviqdJ5VSeTUY8PatCNBKeaREvo2OqdplND/o=" via-port "1-10" with-interface { 03:01:02 03:00:00 03:00:00 } with-connect-type "hotplug"
-        allow id 174c:2074 serial "" name "ASM107x" hash "YTn13nEqBf/5/WIma/Qv+qGbdHqvXRdsLR1lCgHDdXQ=" parent-hash "jEP/6WzviqdJ5VSeTUY8PatCNBKeaREvo2OqdplND/o=" via-port "1-4" with-interface { 09:00:01 09:00:02 } with-connect-type "hardwired"
+        allow id 056e:00e4 serial "" name "ELECOM BlueLED Mouse" hash "RIglSjpBvzvt6hj8DgwXrivAD4mu/tw/iXnpVuUFYPc=" parent-hash "jEP/6WzviqdJ5VSeTUY8PatCNBKeaREvo2OqdplND/o=" with-interface { 03:01:02 03:00:00 03:00:00 } with-connect-type "hotplug"
+        allow id 174c:2074 serial "" name "ASM107x" hash "YTn13nEqBf/5/WIma/Qv+qGbdHqvXRdsLR1lCgHDdXQ=" parent-hash "jEP/6WzviqdJ5VSeTUY8PatCNBKeaREvo2OqdplND/o=" with-interface { 09:00:01 09:00:02 } with-connect-type "hardwired"
         allow id 174c:3074 serial "" name "ASM107x" hash "/IkXwFmioUm1EGuQOP70FeVlV7j930SIZqSijMjn1Tk=" parent-hash "prM+Jby/bFHCn2lNjQdAMbgc6tse3xVx+hZwjOPHSdQ=" via-port "2-4" with-interface 09:00:00 with-connect-type "hardwired"
-        allow id 1c4f:0027 serial "" name "USB Keyboard" hash "gzB3pSPYLpoEhKQLsEli7se1ynHnm05rUm+V9bnsOlY=" parent-hash "jEP/6WzviqdJ5VSeTUY8PatCNBKeaREvo2OqdplND/o=" via-port "1-9" with-interface { 03:01:01 03:00:00 } with-connect-type "hotplug"
-        allow id 26ce:01a2 serial "A02019100900" name "LED Controller" hash "WdEIqOrWT1p6UWeWoigz6TB5lGjHJbCt5NwfiEgXKw0=" parent-hash "jEP/6WzviqdJ5VSeTUY8PatCNBKeaREvo2OqdplND/o=" via-port "1-11" with-interface 03:00:00 with-connect-type "hardwired"
+        allow id 1c4f:0027 serial "" name "USB Keyboard" hash "gzB3pSPYLpoEhKQLsEli7se1ynHnm05rUm+V9bnsOlY=" parent-hash "jEP/6WzviqdJ5VSeTUY8PatCNBKeaREvo2OqdplND/o=" with-interface { 03:01:01 03:00:00 } with-connect-type "hotplug"
+        allow id 26ce:01a2 serial "A02019100900" name "LED Controller" hash "WdEIqOrWT1p6UWeWoigz6TB5lGjHJbCt5NwfiEgXKw0=" parent-hash "jEP/6WzviqdJ5VSeTUY8PatCNBKeaREvo2OqdplND/o=" with-interface 03:00:00 with-connect-type "hardwired"
       '';
     };
   };
