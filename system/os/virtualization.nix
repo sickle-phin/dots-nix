2590121a4a0f3ec28776a7bc516b9f51fc1d0278
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2024-10-14 03:02:18 +0900

    virtualization: add usb support

--- system/os/virtualization.nix
+++ system/os/virtualization.nix
@@ -1,8 +1,11 @@
 { pkgs, ... }:
 {
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu.package = pkgs.qemu_kvm;
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu.package = pkgs.qemu_kvm;
+    };
+    spiceUSBRedirection.enable = true;
   };
   programs.virt-manager.enable = true;
 }
