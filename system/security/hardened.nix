d95b4a23da9479c87cb2a4753893b90dd27a62b4
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2024-10-18 22:10:05 +0900

    feat: add blacklisted kernel modules

--- system/security/hardened.nix
+++ system/security/hardened.nix
@@ -1,3 +1,4 @@
+{ config, lib, ... }:
 {
   boot = {
     kernel.sysctl = {
@@ -76,39 +77,71 @@
       "vsyscall=none"
     ];
 
-    blacklistedKernelModules = [
-      # Obscure network protocols
-      "ax25"
-      "netrom"
-      "rose"
+    blacklistedKernelModules = lib.concatLists [
+      [
+        # Obscure network protocols
+        "af_802154" # IEEE 802.15.4
+        "appletalk" # Appletalk
+        "atm" # ATM
+        "ax25" # Amateur X.25
+        "can" # Controller Area Network
+        "dccp" # Datagram Congestion Control Protocol
+        "decnet" # DECnet
+        "econet" # Econet
+        "ipx" # Internetwork Packet Exchange
+        "n-hdlc" # High-level Data Link Control
+        "netrom" # NetRom
+        "p8022" # IEEE 802.3
+        "p8023" # Novell raw IEEE 802.3
+        "psnap" # SubnetworkAccess Protocol
+        "rds" # Reliable Datagram Sockets
+        "rose" # ROSE
+        "sctp" # Stream Control Transmission Protocol
+        "tipc" # Transparent Inter-Process Communication
+        "x25" # X.25
 
-      # Old or rare or insufficiently audited filesystems
-      "adfs"
-      "affs"
-      "bfs"
-      "befs"
-      "cramfs"
-      "efs"
-      "erofs"
-      "exofs"
-      "freevxfs"
-      "f2fs"
-      "hfs"
-      "hpfs"
-      "jfs"
-      "minix"
-      "nilfs2"
-      "omfs"
-      "qnx4"
-      "qnx6"
-      "sysv"
-      "ufs"
+        # Old or rare or insufficiently audited filesystems
+        "adfs" # Active Directory Federation Services
+        "affs" # Amiga Fast File System
+        "befs" # "Be File System"
+        "bfs" # BFS, used by SCO UnixWare OS for the /stand slice
+        "cramfs" # compressed ROM/RAM file system
+        "cifs" # smb (Common Internet File System)
+        "efs" # Extent File System
+        "erofs" # Enhanced Read-Only File System
+        "exofs" # EXtended Object File System
+        "freevxfs" # Veritas filesystem driver
+        "f2fs" # Flash-Friendly File System
+        "vivid" # Virtual Video Test Driver (unnecessary)
+        "gfs2" # Global File System 2
+        "hpfs" # High Performance File System (used by OS/2)
+        "hfs" # Hierarchical File System (Macintosh)
+        "hfsplus" # " same as above, but with extended attributes
+        "jffs2" # Journalling Flash File System (v2)
+        "jfs" # Journaled File System - only useful for VMWare sessions
+        "ksmbd" # SMB3 Kernel Server
+        "minix" # minix fs - used by the minix OS
+        "nfsv3" # " (v3)
+        "nfsv4" # Network File System (v4)
+        "nfs" # Network File System
+        "nilfs2" # New Implementation of a Log-structured File System
+        "omfs" # Optimized MPEG Filesystem
+        "qnx4" # extent-based file system used by the QNX4 and QNX6 OSes
+        "qnx6" # ^
+        "squashfs" # compressed read-only file system (used by live CDs)
+        "sysv" # implements all of Xenix FS, SystemV/386 FS and Coherent FS.
+        "udf" # https://docs.kernel.org/5.15/filesystems/udf.html
+      ]
+      (lib.optionals (!config.myOptions.hasBluetooth) [
+        "bluetooth"
+        "btusb" # bluetooth dongles
+      ])
     ];
   };
 
   security = {
     forcePageTableIsolation = true;
     protectKernelImage = true;
-    virtualisation.flushL1DataCache ="always";
+    virtualisation.flushL1DataCache = "always";
   };
 }
