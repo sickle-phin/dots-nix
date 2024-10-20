7e732ba17527e81c493a7fc4db7aa6215bce55ea
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2024-10-20 17:33:07 +0900

    refactor: remove unnecessary substituter

--- flake.nix
+++ flake.nix
@@ -4,11 +4,9 @@
   nixConfig = {
     extra-substituters = [
       "https://nix-community.cachix.org"
-      "https://nix-gaming.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
     ];
   };
 
