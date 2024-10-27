5e845d28f28673ee17b0047414bbf14b3d11bfdc
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2024-10-27 14:40:22 +0900

    fix: catppuccin theme for fcitx5

--- home/desktop/fcitx5/default.nix
+++ home/desktop/fcitx5/default.nix
@@ -7,7 +7,6 @@
         pkgs.fcitx5-mozc
         pkgs.fcitx5-gtk
       ];
-      catppuccin.enable = true;
     };
   };
 
@@ -20,5 +19,21 @@
       source = ./config;
       force = true;
     };
+    "fcitx5/conf/classicui.conf".text = "Theme=catppuccin-mocha-mauve";
+  };
+
+  xdg.dataFile = {
+    "fcitx5/themes/catppuccin-mocha-mauve" =
+      let
+        fcitx5 = pkgs.fetchFromGitHub {
+          owner = "catppuccin";
+          repo = "fcitx5";
+          rev = "3471b918d4b5aab2d3c3dd9f2c3b9c18fb470e8e";
+          sha256 = "sha256-1IqFVTEY6z8yNjpi5C+wahMN1kpt0OJATy5echjPXmc=";
+        };
+      in
+      {
+        source = "${fcitx5}/src/catppuccin-mocha-mauve";
+      };
   };
 }
--- home/desktop/theme/fonts.nix
+++ home/desktop/theme/fonts.nix
@@ -1,9 +1,9 @@
 { pkgs, ... }:
 let
-  appleColorEmoji = "${pkgs.fetchurl {
+  appleColorEmoji = pkgs.fetchurl {
     url = "https://github.com/samuelngs/apple-emoji-linux/releases/download/v17.4/AppleColorEmoji.ttf";
     sha256 = "sha256-SG3JQLybhY/fMX+XqmB/BKhQSBB0N1VRqa+H6laVUPE=";
-  }}";
+  };
 in
 {
   xdg.dataFile = {
