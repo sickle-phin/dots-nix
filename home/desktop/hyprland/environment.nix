d4d7043931d86d3e9efa0d32341e6e732f0f9c81
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2024-11-23 04:30:09 +0900

    fix: QT environment variable

--- home/desktop/hyprland/environment.nix
+++ home/desktop/hyprland/environment.nix
@@ -4,7 +4,7 @@
     "uwsm/env".text = ''
       export NIXOS_OZONE_WL=1
       export GDK_BACKEND=wayland,x11
-      export QT_QPA_PLATFORM=wayland;xcb
+      export QT_QPA_PLATFORM="wayland;xcb"
       export QT_SCALE_FACTOR=1.2
       export QT_AUTO_SCREEN_SCALE_FACTOR=1
       export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
