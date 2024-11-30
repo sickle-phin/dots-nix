c08dfb8326f078f5564f4043d9a79bd9ed757606
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2024-12-01 03:06:17 +0900

    fix: add missing environment variables to uwsm

--- home/desktop/hyprland/environment.nix
+++ home/desktop/hyprland/environment.nix
@@ -3,17 +3,21 @@
   xdg.configFile = {
     "uwsm/env".text = ''
       export NIXOS_OZONE_WL=1
-      export GDK_BACKEND=wayland,x11
+      export XMODIFIERS=@im=fcitx
+      export QT_IM_MODULE=fcitx
       export QT_QPA_PLATFORM="wayland;xcb"
       export QT_SCALE_FACTOR=1.2
       export QT_AUTO_SCREEN_SCALE_FACTOR=1
       export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+      export GDK_BACKEND=wayland,x11
       export MOZ_ENABLE_WAYLAND=1
       export SDL_VIDEODRIVER=wayland
       export CLUTTER_BACKEND=wayland
       export SWWW_TRANSITION=center
       export SWWW_TRANSITION_FPS=${toString osConfig.myOptions.maxFramerate}
       export SWWW_TRANSITION_STEP=255
+      export XCURSOR_THEME=catppuccin-mocha-dark-cursors
+      export XCURSOR_SIZE=32
     '';
 
     "uwsm/env-hyprland".text = ''
