6f31f4eba1ec7fa4f9a4d5753fd12a51ea37dd86
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2024-12-24 23:18:45 +0900

    chore: move fcitx5 NixOS module

--- home/desktop/fcitx5/default.nix
+++ home/desktop/fcitx5/default.nix
@@ -1,21 +1,4 @@
-{ lib, pkgs, ... }:
 {
-  home.sessionVariables = {
-    GTK_IM_MODULE = lib.mkForce "";
-  };
-
-  i18n.inputMethod = {
-    enabled = "fcitx5";
-    fcitx5 = {
-      addons = [
-        pkgs.fcitx5-mozc-ut
-      ];
-      catppuccin.enable = true;
-    };
-  };
-
-  systemd.user.services.fcitx5-daemon.Unit.After = lib.mkForce "graphical-session.target";
-
   xdg.configFile = {
     "fcitx5/profile" = {
       source = ./profile;
