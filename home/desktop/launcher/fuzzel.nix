75ee551697e8b3caad444ea78b2439e4fd7f3673
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2024-12-28 06:44:20 +0900

    fix(fuzzel): missing option to launch terminal

--- home/desktop/launcher/fuzzel.nix
+++ home/desktop/launcher/fuzzel.nix
@@ -9,7 +9,7 @@
         use-bold = true;
         placeholder = "Search...";
         prompt = "\">>  \"";
-        terminal = "ghostty";
+        terminal = "ghostty -e";
         launch-prefix = "env LANG=ja_JP.UTF-8 uwsm-app -- ";
         lines = 10;
         width = 45;
