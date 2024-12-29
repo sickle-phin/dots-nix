9f67746f0909e78f28867cbccacbe9fc9d704847
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2024-12-29 19:38:57 +0900

    fix: remove extra space

--- home/desktop/scripts/set-option.nix
+++ home/desktop/scripts/set-option.nix
@@ -20,21 +20,21 @@ let
             exit 0
         fi
 
-        if [[ $pick = "''${CONFIGS [ 0 ]}" ]]; then
+        if [[ $pick = "''${CONFIGS[0]}" ]]; then
             border_size=$(hyprctl getoption general:border_size | head -n 1 | awk '{print $2}')
             if [[ border_size -eq 0 ]]; then
                 hyprctl keyword general:border_size 3
             else
                 hyprctl keyword general:border_size 0
             fi
-        elif [[ $pick = "''${CONFIGS [ 1 ]}" ]]; then
+        elif [[ $pick = "''${CONFIGS[1]}" ]]; then
             blur=$(hyprctl getoption decoration:blur:enabled | head -n 1 | awk '{print $2}')
             if [[ blur -eq 0 ]]; then
                 hyprctl keyword decoration:blur:enabled 1
             else
                 hyprctl keyword decoration:blur:enabled 0
             fi
-        elif [[ $pick = "''${CONFIGS [ 2 ]}" ]]; then
+        elif [[ $pick = "''${CONFIGS[2]}" ]]; then
             shadow=$(hyprctl getoption decoration:drop_shadow | head -n 1 | awk '{print $2}')
             if [[ shadow -eq 0 ]]; then
                 hyprctl keyword decoration:drop_shadow 1
