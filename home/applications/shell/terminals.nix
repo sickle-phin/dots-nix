e80dc788f42bb834848638a98ed0c8ccebbdfe5c
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2025-01-09 03:41:47 +0900

    feat(wezterm): add config

--- home/applications/shell/terminals.nix
+++ home/applications/shell/terminals.nix
@@ -29,12 +29,17 @@
 
       config.enable_tab_bar = false
       config.font = wezterm.font_with_fallback({
-          { family = "Moralerspace Neon HW" },
+          {
+            family = "Moralerspace Neon HW",
+            harfbuzz_features = { 'calt', 'liga', 'ss01', 'ss02', 'ss03', 'ss04', 'ss05', 'ss06', 'ss07', 'ss08', 'ss09' }
+          },
           { family = "Apple Color Emoji" },
       })
       config.font_size = 18.0
       config.front_end = "WebGpu"
       config.term = "wezterm"
+      config.underline_position="-3pt"
+      config.underline_thickness="2pt"
       config.webgpu_power_preference = "HighPerformance"
       config.window_background_opacity = 0.85
 
