bad5a0cf04f7e6b43229774a046ec66570c257b9
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2025-01-13 00:13:40 +0900

    feat: launch Steam client

--- home/desktop/hyprland/execs.nix
+++ home/desktop/hyprland/execs.nix
@@ -1,25 +1,34 @@
 {
   config,
   lib,
+  osConfig,
   pkgs,
   ...
 }:
+let
+  inherit (lib.lists) optionals;
+  inherit (lib.meta) getExe;
+in
 {
   wayland.windowManager.hyprland.settings = {
-    exec-once = [
-      "hyprctl setcursor \"$(cat ${config.xdg.cacheHome}/theme/cursor_theme)\" \"$(cat ${config.xdg.cacheHome}/theme/hyprcursor_size)\""
-      "uwsm finalize"
-      "uwsm app -- swww-daemon"
-      "hyprpanel"
-      "systemctl --user enable --now hyprpolkitagent.service"
-      "uwsm app -- hyprsunset"
-      "sleep 8 && uwsm app -- slack --startup"
-      "uwsm app -- ${lib.getExe pkgs.wl-clip-persist} --clipboard regular"
-      "sleep 8 && [ ! -e \"${config.xdg.cacheHome}/theme/border.conf\" ] && notify-send -u critical -i ${../icons/hyprland.png} \"Hyprland\" \"Press mod+T to select theme\!\!\""
-    ];
+    exec-once =
+      [
+        "hyprctl setcursor \"$(cat ${config.xdg.cacheHome}/theme/cursor_theme)\" \"$(cat ${config.xdg.cacheHome}/theme/hyprcursor_size)\""
+        "uwsm finalize"
+        "uwsm app -- swww-daemon"
+        "hyprpanel"
+        "systemctl --user enable --now hyprpolkitagent.service"
+        "uwsm app -- ${getExe pkgs.hyprsunset}"
+        "sleep 8 && uwsm app -- ${getExe pkgs.slack} --startup"
+        "uwsm app -- ${getExe pkgs.wl-clip-persist} --clipboard regular"
+        "sleep 8 && [ ! -e \"${config.xdg.cacheHome}/theme/border.conf\" ] && notify-send -u critical -i ${../icons/hyprland.png} \"Hyprland\" \"Press mod+T to select theme\!\!\""
+      ]
+      ++ optionals osConfig.myOptions.enableGaming [
+        "sleep 8 && uwsm app -- ${getExe pkgs.steam} -silent"
+      ];
 
     exec-shutdown = [
-      "${lib.getExe pkgs.cliphist} wipe"
+      "${getExe pkgs.cliphist} wipe"
     ];
   };
 }
