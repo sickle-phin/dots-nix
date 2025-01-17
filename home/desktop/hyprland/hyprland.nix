2e4f45275a119fd7288d7f73c155c0787472deab
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2025-01-17 17:38:37 +0900

    chore: disable hardware cursor on nvidia gpu

--- home/desktop/hyprland/hyprland.nix
+++ home/desktop/hyprland/hyprland.nix
@@ -56,7 +56,7 @@ in
       };
 
       cursor = {
-        no_hardware_cursors = false;
+        no_hardware_cursors = 2;
         min_refresh_rate = lib.mkIf (host == "irukaha") 48;
         default_monitor = lib.mkIf (host == "irukaha" || host == "labo") "DP-1";
         use_cpu_buffer = true;
