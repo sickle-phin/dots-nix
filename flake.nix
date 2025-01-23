56c3c87315e1243e6aba297c5f2a5d73d399a22b
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2025-01-24 00:43:23 +0900

    chore: update flake.lock

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.1";
+      url = "github:nix-community/lanzaboote/v0.4.2";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
