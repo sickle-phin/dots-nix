8c42000e5c9a068da1f68b3972b4e6f0049b0dca
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2025-05-30 00:38:49 +0900

    fix: change dnscrypt-proxy2 cache file

--- system/os/networking.nix
+++ system/os/networking.nix
@@ -68,7 +68,7 @@ in
             "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
             "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
           ];
-          cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+          cache_file = "/var/cache/dnscrypt-proxy/public-resolvers.md";
           minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
         };
 
