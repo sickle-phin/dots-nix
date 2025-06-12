b28502754f8272458ca9a57e2ccbf8b396426e18
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2025-06-12 18:55:17 +0900

    feat: add console catppuccin theme

--- system/os/console.nix
+++ system/os/console.nix
@@ -1,11 +1,32 @@
 {
   pkgs,
   config,
-  lib,
   ...
 }:
 {
-  console.earlySetup = true;
+  console = {
+    earlySetup = true;
+    colors = [
+      "1e1e2e"
+      "f38ba8"
+      "a6e3a1"
+      "f9e2af"
+      "89b4fa"
+      "f5c2e7"
+      "94e2d5"
+      "bac2de"
+
+      "585b70"
+      "f38ba8"
+      "a6e3a1"
+      "f9e2af"
+      "89b4fa"
+      "f5c2e7"
+      "94e2d5"
+      "a6adc8"
+    ];
+
+  };
 
   services.kmscon = {
     enable = true;
