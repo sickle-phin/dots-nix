eeebe94751800b22abb1b9c87091aa1aa4474a34
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2025-07-06 22:41:00 +0900

    fix: greetd launch failure

--- system/os/displaymanager.nix
+++ system/os/displaymanager.nix
@@ -3,7 +3,6 @@
   services = {
     greetd = {
       enable = true;
-      package = pkgs.greetd.tuigreet;
       settings = {
         default_session = {
           command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-session --asterisks";
