d6049bf48d8dddca285a92476c99d4f04befea3a
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2025-07-27 05:32:36 +0900

    chore: adjust environment variables for proton

--- home/desktop/hyprland/env.nix
+++ home/desktop/hyprland/env.nix
@@ -25,10 +25,8 @@
       export DXVK_HDR=1
       export ENABLE_HDR_WSI=1
       export GRIMBLAST_HIDE_CURSOR=0
-      export PROTON_ENABLE_WAYLAND=1
+      export PROTON_ENABLE_WAYLAND=0
       export PROTON_ENABLE_HDR=1
-      export PROTON_USE_NTSYNC=1
-      export PROTON_USE_WOW64=1
     '';
 
     "uwsm/env-hyprland".text = ''
