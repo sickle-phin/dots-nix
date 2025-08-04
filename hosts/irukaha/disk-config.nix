1e9ebba5823990d5c0b5e0933d86fa1fc6c8a39f
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2025-08-05 00:02:22 +0900

    refactor

--- hosts/irukaha/disk-config.nix
+++ hosts/irukaha/disk-config.nix
@@ -1,13 +1,4 @@
 {
-  imports = [
-    "${
-      builtins.fetchTarball {
-        url = "https://github.com/nix-community/disko/archive/refs/tags/v1.11.0.tar.gz";
-        sha256 = "13brimg7z7k9y36n4jc1pssqyw94nd8qvgfjv53z66lv4xkhin92";
-      }
-    }/module.nix"
-  ];
-
   fileSystems."/persistent".neededForBoot = true;
 
   disko.devices = {
