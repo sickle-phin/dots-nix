d92d70b63370ad5e4c23b0add4c0f44c8f33f5d6
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2025-09-06 23:09:11 +0900

    chore: move apple emoji to system font

--- home/desktop/theme/default.nix
+++ home/desktop/theme/default.nix
@@ -1,7 +1,6 @@
 {
   imports = [
     ./dconf.nix
-    ./fonts.nix
     ./fuzzel
     ./gtk.nix
     ./qt.nix
--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,17 +1,33 @@
 { pkgs, ... }:
 {
   fonts = {
-    packages = builtins.attrValues {
-      inherit (pkgs)
-        noto-fonts-cjk-serif
-        noto-fonts-cjk-sans
-        noto-fonts-color-emoji
-        mona-sans
-        moralerspace-hw
-        font-awesome
-        ;
-      inherit (pkgs.nerd-fonts) symbols-only;
-    };
+    packages =
+      let
+        apple-color-emoji = pkgs.stdenvNoCC.mkDerivation {
+          name = "Apple Color Emoji";
+          src = pkgs.fetchurl {
+            url = "https://github.com/samuelngs/apple-emoji-linux/releases/latest/download/AppleColorEmoji.ttf";
+            sha256 = "sha256-pP0He9EUN7SUDYzwj0CE4e39SuNZ+SVz7FdmUviF6r0=";
+          };
+          phases = [ "installPhase" ];
+          installPhase = ''
+            mkdir -p $out/share/fonts/truetype
+            cp $src $out/share/fonts/truetype/AppleColorEmoji.ttf
+          '';
+        };
+      in
+      builtins.attrValues {
+        inherit (pkgs)
+          noto-fonts-cjk-serif
+          noto-fonts-cjk-sans
+          noto-fonts-color-emoji
+          mona-sans
+          moralerspace-hw
+          font-awesome
+          ;
+        inherit (pkgs.nerd-fonts) symbols-only;
+        inherit apple-color-emoji;
+      };
     fontDir = {
       enable = true;
       decompressFonts = true;
