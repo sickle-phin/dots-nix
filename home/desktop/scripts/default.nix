a793ac77e4623cb2ecfad763491a3b05f43e5120
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2025-09-22 02:51:41 +0900

    fix: cursor theme

--- home/desktop/scripts/default.nix
+++ home/desktop/scripts/default.nix
@@ -2,7 +2,6 @@
   imports = [
     ./cliphist-fuzzel-img.nix
     ./ocr.nix
-    ./select-wallpaper.nix
     ./set-option.nix
     ./tasks.nix
     ./theme.nix
--- home/desktop/theme/gtk.nix
+++ home/desktop/theme/gtk.nix
@@ -22,4 +22,32 @@
         papirus-icon-theme
         ;
     };
+
+  dconf = {
+    enable = true;
+    settings = {
+      "org/gnome/desktop/interface" = {
+        font-name = "Noto Sans CJK JP 11";
+        cursor-size = 34;
+      };
+      "org/gnome/nm-applet" = {
+        disable-disconnected-notifications = true;
+        disable-connected-notifications = true;
+        suppress-wireless-networks-available = true;
+      };
+    };
+  };
+
+  specialisation = {
+    dark.configuration.dconf.settings."org/gnome/desktop/interface" = {
+      cursor-theme = "catppuccin-mocha-dark-cursors";
+      gtk-theme = "catppuccin-mocha-pink-standard+normal";
+      icon-theme = "Papirus-Dark";
+    };
+    light.configuration.dconf.settings."org/gnome/desktop/interface" = {
+      cursor-theme = "catppuccin-latte-light-cursors";
+      gtk-theme = "catppuccin-latte-pink-standard+normal";
+      icon-theme = "Papirus-Light";
+    };
+  };
 }
