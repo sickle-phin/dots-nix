b332e5ff7dbf3984e20010db2d75744ce9ff301c
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2025-10-10 02:43:30 +0900

    feat: add vesktop config

--- home/applications/gui.nix
+++ home/applications/gui.nix
@@ -11,7 +11,6 @@
       slack
       teams-for-linux
       thunderbird
-      vesktop
       ;
   };
 
@@ -39,6 +38,36 @@
           ;
       };
     };
+    vesktop = {
+      enable = true;
+      settings = {
+        hardwareVideoAcceleration = true;
+        enableMenu = true;
+      };
+      vencord = {
+        useSystem = true;
+        settings = {
+          enabledThemes = [ "catppuccin.theme.css" ];
+          autoUpdate = false;
+          autoUpdateNotification = false;
+          transparent = true;
+          plugins = {
+            AlwaysAnimate.enabled = true;
+            MessageLogger = {
+              enabled = true;
+              ignoreSelf = true;
+            };
+            FakeNitro.enabled = true;
+          };
+        };
+        themes."catppuccin.theme" = ''
+          @import url("https://catppuccin.github.io/discord/dist/catppuccin-mocha-${osConfig.myOptions.catppuccin.accent.dark}.theme.css")
+          (prefers-color-scheme: dark);
+          @import url("https://catppuccin.github.io/discord/dist/catppuccin-latte-${osConfig.myOptions.catppuccin.accent.light}.theme.css")
+          (prefers-color-scheme: light);
+        '';
+      };
+    };
   };
 
   specialisation = {
@@ -52,6 +81,10 @@
         url = "https://raw.githubusercontent.com/catppuccin/mpv/refs/heads/main/themes/mocha/pink.conf";
         sha256 = "sha256-UOoTa8KsRweQuPnjqum+mvJOc93u7bMleMdveQTI+Rg=";
       }}";
+      vesktop.settings = {
+        splashColor = "rgb(205, 214, 244)";
+        splashBackground = "rgb(30, 30, 46)";
+      };
     };
     light.configuration.programs = {
       imv.settings.options = {
@@ -63,6 +96,10 @@
         url = "https://raw.githubusercontent.com/catppuccin/mpv/refs/heads/main/themes/latte/pink.conf";
         sha256 = "sha256-785OOD3/K2lTuno6jMYGKueeg4Q45pcg4eims5yMnqc=";
       }}";
+      vesktop.settings = {
+        splashColor = "rgb(76, 79, 105)";
+        splashBackground = "rgb(239, 241, 245)";
+      };
     };
   };
 }
