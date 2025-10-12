088c87fe9c61eccfdb3416ead4b011bed83a0dcc
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2025-10-13 02:52:13 +0900

    edit disko config

--- hosts/pink/default.nix
+++ hosts/pink/default.nix
@@ -3,7 +3,7 @@
     ../../modules
     "${
       builtins.fetchTarball {
-        url = "https://github.com/nix-community/disko/archive/refs/tags/v1.12.0.tar.gz";
+        url = "https://github.com/nix-community/disko/archive/refs/tags/latest.tar.gz";
         sha256 = "0wbx518d2x54yn4xh98cgm65wvj0gpy6nia6ra7ns4j63hx14fkq";
       }
     }/module.nix"
