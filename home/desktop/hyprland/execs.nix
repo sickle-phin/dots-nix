8e7f0f22e5e453db69d1f37271538c43e2626383
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2025-11-14 01:19:26 +0900

    fix: slack launch

--- home/desktop/hyprland/execs.nix
+++ home/desktop/hyprland/execs.nix
@@ -12,7 +12,7 @@ in
 {
   wayland.windowManager.hyprland.settings = {
     exec-once = [
-      "uwsm-app -- ${getExe pkgs.slack} --startup"
+      "sleep 3 && uwsm app -- ${getExe pkgs.slack} --startup"
       "uwsm app -- ${getExe pkgs.wl-clip-persist} --clipboard regular"
       "mkdir -p ${config.home.homeDirectory}/Videos/Screencasts"
       "systemctl start --user app-com.mitchellh.ghostty.service"
