908acbc3fba1c0651b44ce4be7f3ec26d8db2b5a
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2025-11-16 05:00:39 +0900

    chore: remove icons

--- home/desktop/scripts/tasks.nix
+++ home/desktop/scripts/tasks.nix
@@ -9,29 +9,29 @@
   home.packages = [
     (pkgs.writeShellScriptBin "update-nixos" ''
       if ! nix profile upgrade --all; then
-        notify-send -a "Update NixOS" -u normal -i "${../icons/NixOS.png}" "NixOS" "update failed"
+        notify-send -a "Update NixOS" -u "normal" -i "distributor-logo-nixos" "NixOS" "update failed"
         exit 1
       fi
       if nix flake update --flake "${config.home.homeDirectory}/dots-nix"; then
-        notify-send -a "Update NixOS" -u low -i "${../icons/NixOS.png}" "NixOS" "update completed"
+        notify-send -a "Update NixOS" -u "low" -i "distributor-logo-nixos" "NixOS" "update completed"
       else
-        notify-send -a "Update NixOS" -u normal -i "${../icons/NixOS.png}" "NixOS" "update failed"
+        notify-send -a "Update NixOS" -u "normal" -i "distributor-logo-nixos" "NixOS" "update failed"
       fi
     '')
 
     (pkgs.writeShellScriptBin "gc-nixos" ''
       if nh clean all; then
-        notify-send -a "Garbage Collection" -u low -i "${../icons/NixOS.png}" "NixOS" "garbage collection completed"
+        notify-send -a "Garbage Collection" -u "low" -i "distributor-logo-nixos" "NixOS" "garbage collection completed"
       else
-        notify-send -a "Garbage Collection" -u normal -i "${../icons/NixOS.png}" "NixOS" "garbage collection failed"
+        notify-send -a "Garbage Collection" -u "normal" -i "distributor-logo-nixos" "NixOS" "garbage collection failed"
       fi
     '')
 
     (pkgs.writeShellScriptBin "rebuild-nixos" ''
       if nh os switch -H "${osConfig.networking.hostName}"; then
-        notify-send -a "Rebuild NixOS" -u low -i "${../icons/NixOS.png}" "NixOS" "rebuild completed"
+        notify-send -a "Rebuild NixOS" -u "low" -i "distributor-logo-nixos" "NixOS" "rebuild completed"
       else
-        notify-send -a "Rebuild NixOS" -u normal -i "${../icons/NixOS.png}" "NixOS" "rebuild failed"
+        notify-send -a "Rebuild NixOS" -u "normal" -i "distributor-logo-nixos" "NixOS" "rebuild failed"
       fi
     '')
 
