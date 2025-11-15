11c0bd6667b9d31ecca98523dbc1eacccb16fa2d
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2025-11-15 19:45:39 +0900

    chore: add app name to notification

--- home/desktop/scripts/ocr.nix
+++ home/desktop/scripts/ocr.nix
@@ -6,10 +6,10 @@ let
     OCR_LANG=$1
     CAPITAL=$(echo "$OCR_LANG" | tr '[:lower:]' '[:upper:]')
     if OCR=$(wl-paste | tesseract - - -l "$OCR_LANG"); then
-        notify-send -u low -i ocrfeeder "OCR Processing: Success" "$CAPITAL text copied to the clipboard"
+        notify-send -a OCR -u low -i ocrfeeder "OCR Processing: Success" "$CAPITAL text copied to the clipboard"
         echo "$OCR" | wl-copy
     else
-        notify-send -u critical -i ocrfeeder "OCR Processing: Failure" "Please copy image to the clipboard"
+        notify-send -a OCR -u critical -i ocrfeeder "OCR Processing: Failure" "Please copy image to the clipboard"
         exit 1
     fi
   '';
--- home/desktop/scripts/tasks.nix
+++ home/desktop/scripts/tasks.nix
@@ -9,29 +9,29 @@
   home.packages = [
     (pkgs.writeShellScriptBin "update-nixos" ''
       if ! nix profile upgrade --all; then
-        notify-send -u normal -i "${../icons/NixOS.png}" "NixOS" "update failed"
+        notify-send -a "Update NixOS" -u normal -i "${../icons/NixOS.png}" "NixOS" "update failed"
         exit 1
       fi
       if nix flake update --flake "${config.home.homeDirectory}/dots-nix"; then
-        notify-send -u low -i "${../icons/NixOS.png}" "NixOS" "update completed"
+        notify-send -a "Update NixOS" -u low -i "${../icons/NixOS.png}" "NixOS" "update completed"
       else
-        notify-send -u normal -i "${../icons/NixOS.png}" "NixOS" "update failed"
+        notify-send -a "Update NixOS" -u normal -i "${../icons/NixOS.png}" "NixOS" "update failed"
       fi
     '')
 
     (pkgs.writeShellScriptBin "gc-nixos" ''
       if nh clean all; then
-        notify-send -u low -i "${../icons/NixOS.png}" "NixOS" "garbage collection completed"
+        notify-send -a "Garbage Collection" -u low -i "${../icons/NixOS.png}" "NixOS" "garbage collection completed"
       else
-        notify-send -u normal -i "${../icons/NixOS.png}" "NixOS" "garbage collection failed"
+        notify-send -a "Garbage Collection" -u normal -i "${../icons/NixOS.png}" "NixOS" "garbage collection failed"
       fi
     '')
 
     (pkgs.writeShellScriptBin "rebuild-nixos" ''
       if nh os switch -H "${osConfig.networking.hostName}"; then
-        notify-send -u low -i "${../icons/NixOS.png}" "NixOS" "rebuild completed"
+        notify-send -a "Rebuild NixOS" -u low -i "${../icons/NixOS.png}" "NixOS" "rebuild completed"
       else
-        notify-send -u normal -i "${../icons/NixOS.png}" "NixOS" "rebuild failed"
+        notify-send -a "Rebuild NixOS" -u normal -i "${../icons/NixOS.png}" "NixOS" "rebuild failed"
       fi
     '')
 
