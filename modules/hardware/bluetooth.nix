1096f426fe92d754cc800f2befcec312fe538db8
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2025-11-18 01:26:08 +0900

    refactor

--- modules/hardware/bluetooth.nix
+++ modules/hardware/bluetooth.nix
@@ -1,28 +1,17 @@
+{ config, ... }:
 {
-  config,
-  lib,
-  ...
-}:
-let
-  inherit (lib.modules) mkIf;
-
-  hasBluetooth = config.myOptions.hasBluetooth;
-in
-{
-  config = mkIf hasBluetooth {
-    hardware = {
-      # To activate Bluetooth, execute "rfkill unlock all"
-      bluetooth = {
-        enable = true;
-        powerOnBoot = false;
-        disabledPlugins = [ "sap" ];
-        settings = {
-          General = {
-            Experimental = true;
-            FastConnectable = true;
-            JustWorksRepairing = "always";
-            MultiProfile = "multiple";
-          };
+  # To activate Bluetooth, execute "rfkill unblock bluetooth"
+  hardware = {
+    bluetooth = {
+      enable = config.myOptions.hasBluetooth;
+      powerOnBoot = false;
+      disabledPlugins = [ "sap" ];
+      settings = {
+        General = {
+          Experimental = true;
+          FastConnectable = true;
+          JustWorksRepairing = "always";
+          MultiProfile = "multiple";
         };
       };
     };
