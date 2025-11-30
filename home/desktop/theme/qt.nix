fa3cf91a8ce8237d290f2d2ab233a69f8e81e363
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2025-11-30 23:11:21 +0900

    fix: qt icon theme

--- home/desktop/theme/qt.nix
+++ home/desktop/theme/qt.nix
@@ -1,13 +1,25 @@
-{ osConfig, pkgs, ... }:
+{ config, lib, ... }:
 let
-  catppuccin-latte = pkgs.catppuccin-kvantum.override {
-    accent = osConfig.myOptions.catppuccin.accent.light;
-    variant = "latte";
-  };
-  catppuccin-mocha = pkgs.catppuccin-kvantum.override {
-    accent = osConfig.myOptions.catppuccin.accent.dark;
-    variant = "mocha";
-  };
+  inherit (lib.modules) mkDefault;
+
+  qt5ct = theme: ''
+    [Appearance]
+    color_scheme_path=${config.xdg.configHome}/qt5ct/colors/matugen.conf
+    custom_palette=true
+    icon_theme=Papirus-${theme}
+    [Fonts]
+    fixed="Noto Sans CJK JP,9,-1,5,50,0,0,0,0,0,Regular"
+    general="Noto Sans CJK JP,9,-1,5,50,0,0,0,0,0,Regular"
+  '';
+  qt6ct = theme: ''
+    [Appearance]
+    color_scheme_path=${config.xdg.configHome}/qt6ct/colors/matugen.conf
+    custom_palette=true
+    icon_theme=Papirus-${theme}
+    [Fonts]
+    fixed="Noto Sans CJK JP,9,-1,5,400,0,0,0,0,0,0,0,0,0,0,1"
+    general="Noto Sans CJK JP,9,-1,5,400,0,0,0,0,0,0,0,0,0,0,1"
+  '';
 in
 {
   qt = {
@@ -16,10 +28,6 @@ in
   };
 
   home = {
-    packages = builtins.attrValues {
-      inherit catppuccin-latte;
-      inherit catppuccin-mocha;
-    };
     sessionVariables = {
       QT_QPA_PLATFORM = "wayland;xcb";
       QT_SCALE_FACTOR = "1.2";
@@ -29,46 +37,22 @@ in
   };
 
   xdg.configFile = {
-    "Kvantum/catppuccin-latte-${osConfig.myOptions.catppuccin.accent.light}".source =
-      "${catppuccin-latte}/share/Kvantum/catppuccin-latte-${osConfig.myOptions.catppuccin.accent.light}";
-    "Kvantum/catppuccin-mocha-${osConfig.myOptions.catppuccin.accent.dark}".source =
-      "${catppuccin-mocha}/share/Kvantum/catppuccin-mocha-${osConfig.myOptions.catppuccin.accent.dark}";
     "hypr/application-style.conf".text = ''
       roundness = 2
       border_width = 2
     '';
-    "qt5ct/qt5ct.conf".text = ''
-      [Appearance]
-      color_scheme_path=/home/sickle-phin/.config/qt5ct/colors/matugen.conf
-      custom_palette=true
-      icon_theme=Papirus-Dark
-      [Fonts]
-      fixed="Noto Sans CJK JP,9,-1,5,50,0,0,0,0,0,Regular"
-      general="Noto Sans CJK JP,9,-1,5,50,0,0,0,0,0,Regular"
-    '';
-    "qt6ct/qt6ct.conf".text = ''
-      [Appearance]
-      color_scheme_path=/home/sickle-phin/.config/qt6ct/colors/matugen.conf
-      custom_palette=true
-      icon_theme=Papirus-Dark
-      [Fonts]
-      fixed="Noto Sans CJK JP,9,-1,5,400,0,0,0,0,0,0,0,0,0,0,1"
-      general="Noto Sans CJK JP,9,-1,5,400,0,0,0,0,0,0,0,0,0,0,1"
-    '';
+    "qt5ct/qt5ct.conf".text = mkDefault (qt5ct "Dark");
+    "qt6ct/qt6ct.conf".text = mkDefault (qt6ct "Dark");
   };
 
   specialisation = {
     dark.configuration.xdg.configFile = {
-      "Kvantum/kvantum.kvconfig".text = ''
-        [General]
-        theme=catppuccin-mocha-${osConfig.myOptions.catppuccin.accent.dark}
-      '';
+      "qt5ct/qt5ct.conf".text = qt5ct "Dark";
+      "qt6ct/qt6ct.conf".text = qt6ct "Dark";
     };
     light.configuration.xdg.configFile = {
-      "Kvantum/kvantum.kvconfig".text = ''
-        [General]
-        theme=catppuccin-latte-${osConfig.myOptions.catppuccin.accent.light}
-      '';
+      "qt5ct/qt5ct.conf".text = qt5ct "Light";
+      "qt6ct/qt6ct.conf".text = qt6ct "Light";
     };
   };
 }
