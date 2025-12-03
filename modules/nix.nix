8ebeaf8ad52e531bd026ccc6b5c9d3076b8d6b1e
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2025-12-03 22:57:53 +0900

    refactor

--- modules/nix.nix
+++ modules/nix.nix
@@ -17,15 +17,10 @@
       http-connections = 50;
       max-jobs = "auto";
       substituters = [
-        "https://cache.nixos.org"
         "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
-      trusted-users = [
-        "root"
       ];
       use-xdg-base-directories = true;
     };
