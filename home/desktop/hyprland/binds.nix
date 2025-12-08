10776e21a8aa598a7500dee27ee20355cee1ea24
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2025-12-09 00:08:22 +0900

    refactor

--- home/desktop/hyprland/binds.nix
+++ home/desktop/hyprland/binds.nix
@@ -1,5 +1,6 @@
 {
   config,
+  inputs,
   lib,
   osConfig,
   pkgs,
@@ -7,6 +8,7 @@
 }:
 let
   inherit (lib.meta) getExe;
+  dms = getExe inputs.dankMaterialShell.packages.${pkgs.stdenv.hostPlatform.system}.dms-shell;
 in
 {
   wayland.windowManager.hyprland.settings = {
@@ -31,21 +33,21 @@ in
       "SUPER_SHIFT, K, movewindow, u"
       "SUPER_SHIFT, J, movewindow, d"
 
-      "SUPER_SHIFT, C, exec, dms color pick -a"
-      "$mod, D, exec, dms ipc call spotlight toggle"
-      "SUPER_SHIFT, E, exec, dms ipc call powermenu toggle"
-      "$mod, M, exec, dms ipc call processlist toggle"
-      "SUPER_SHIFT, N, exec, dms ipc call notepad toggle"
-      "$mod, N, exec, dms ipc call notifications toggle"
-      "$mod, T, exec, dms ipc call theme toggle"
-      "$mod, V, exec, dms ipc call clipboard toggle"
-      "$mod, W, exec, dms ipc call dankdash wallpaper"
-      "$mod, comma, exec, dms ipc call settings toggle"
-      "$mod, TAB, exec, dms ipc call hypr toggleOverview"
-      "SUPER_SHIFT, S, exec, dms screenshot --no-file"
-      ", Print, exec, dms screenshot full --stdout | ${getExe pkgs.satty} -f -"
-      "Shift, Print, exec, dms screenshot window --stdout | ${getExe pkgs.satty} -f -"
-      "$mod, Print, exec, pkill wl-screenrec || uwsm-app -- wl-screenrec -f \"${config.home.homeDirectory}/Videos/Screencasts/$(date +%Y%m%d_%H%M%S).mp4\""
+      "SUPER_SHIFT, C, exec, ${dms} color pick -a"
+      "$mod, D, exec, ${dms} ipc call spotlight toggle"
+      "SUPER_SHIFT, E, exec, ${dms} ipc call powermenu toggle"
+      "$mod, M, exec, ${dms} ipc call processlist toggle"
+      "SUPER_SHIFT, N, exec, ${dms} ipc call notepad toggle"
+      "$mod, N, exec, ${dms} ipc call notifications toggle"
+      "$mod, T, exec, ${dms} ipc call theme toggle"
+      "$mod, V, exec, ${dms} ipc call clipboard toggle"
+      "$mod, W, exec, ${dms} ipc call dankdash wallpaper"
+      "$mod, comma, exec, ${dms} ipc call settings toggle"
+      "$mod, TAB, exec, ${dms} ipc call hypr toggleOverview"
+      "SUPER_SHIFT, S, exec, ${dms} screenshot --no-file"
+      ", Print, exec, ${dms} screenshot full --stdout | ${getExe pkgs.satty} -f -"
+      "Shift, Print, exec, ${dms} screenshot window --stdout | ${getExe pkgs.satty} -f -"
+      "$mod, Print, exec, pkill wl-screenrec || uwsm-app -- ${getExe pkgs.wl-screenrec} -f \"${config.home.homeDirectory}/Videos/Screencasts/$(date +%Y%m%d_%H%M%S).mp4\""
 
       "$mod, mouse_down, workspace, e+1"
       "$mod, mouse_up, workspace, e-1"
@@ -82,27 +84,27 @@ in
     ];
 
     bindel = [
-      ", XF86AudioRaiseVolume, exec, dms ipc call audio increment 5"
-      ", XF86AudioLowerVolume, exec, dms ipc call audio decrement 5"
-      "$mod, F6, exec, dms ipc call audio increment 5"
-      "$mod, F5, exec, dms ipc call audio decrement 5"
-      ", XF86MonBrightnessUp, exec, dms ipc call brightness increment 5 \"\""
-      ", XF86MonBrightnessDown, exec, dms ipc call brightness decrement 5 \"\""
+      ", XF86AudioRaiseVolume, exec, ${dms} ipc call audio increment 5"
+      ", XF86AudioLowerVolume, exec, ${dms} ipc call audio decrement 5"
+      "$mod, F6, exec, ${dms} ipc call audio increment 5"
+      "$mod, F5, exec, ${dms} ipc call audio decrement 5"
+      ", XF86MonBrightnessUp, exec, ${dms} ipc call brightness increment 5 \"\""
+      ", XF86MonBrightnessDown, exec, ${dms} ipc call brightness decrement 5 \"\""
     ];
 
     bindl = [
-      ", XF86AudioMute, exec, dms ipc call audio mute"
-      "$mod, F4, exec, exec, dms ipc call audio mute"
-      ", XF86AudioMicMute, exec, dms ipc call audio micmute"
-      ", XF86AudioPause, exec, dms ipc call mpris playPause"
-      ", XF86AudioPlay, exec, dms ipc call mpris playPause"
-      ", XF86AudioPrev, exec, playerctl position -5"
-      ", XF86AudioNext, exec, playerctl position +5"
+      ", XF86AudioMute, exec, ${dms} ipc call audio mute"
+      "$mod, F4, exec, exec, ${dms} ipc call audio mute"
+      ", XF86AudioMicMute, exec, ${dms} ipc call audio micmute"
+      ", XF86AudioPause, exec, ${dms} ipc call mpris playPause"
+      ", XF86AudioPlay, exec, ${dms} ipc call mpris playPause"
+      ", XF86AudioPrev, exec, ${getExe pkgs.playerctl} position -5"
+      ", XF86AudioNext, exec, ${getExe pkgs.playerctl} position +5"
     ];
 
     bindlo = [
-      ", XF86AudioPrev, exec, dms ipc call mpris previous"
-      ", XF86AudioNext, exec, dms ipc call mpris next"
+      ", XF86AudioPrev, exec, ${dms} ipc call mpris previous"
+      ", XF86AudioNext, exec, ${dms} ipc call mpris next"
     ];
 
     bindm = [
