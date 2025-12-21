8305d58497e3c5d386c1abf896bc35fd5aacaf1f
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2025-12-21 21:22:37 +0900

    refactor

--- home/desktop/scripts/default.nix
+++ home/desktop/scripts/default.nix
@@ -1,7 +1,6 @@
 {
   imports = [
     ./ocr.nix
-    ./set-option.nix
     ./tasks.nix
     ./theme.nix
   ];
--- home/desktop/scripts/theme.nix
+++ home/desktop/scripts/theme.nix
@@ -1,14 +1,21 @@
-{ lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   inherit (lib.meta) getExe;
+  inherit (lib.meta) getExe';
+  hyprctl = "${getExe' config.wayland.windowManager.hyprland.package "hyprctl"}";
 
   init-theme = pkgs.writeShellScriptBin "init-theme" ''
     theme=$(${getExe pkgs.dconf} read /org/gnome/desktop/interface/color-scheme)
     if [[ $theme = "'default'" ]]; then
-        hyprctl setcursor "catppuccin-latte-light-cursors" 37
+        ${hyprctl} setcursor "catppuccin-latte-light-cursors" 37
         ~/.config/specialisation/light/activate
     else
-        hyprctl setcursor "catppuccin-mocha-dark-cursors" 37
+        ${hyprctl} setcursor "catppuccin-mocha-dark-cursors" 37
         ~/.config/specialisation/dark/activate
     fi
   '';
@@ -16,13 +23,13 @@ let
   mode-changed-hook = pkgs.writeShellScriptBin "mode-changed-hook" ''
     theme="$2"
     if [[ $theme = "light" ]]; then
-        hyprctl setcursor "catppuccin-latte-light-cursors" 37
+        ${hyprctl} setcursor "catppuccin-latte-light-cursors" 37
         ${getExe pkgs.dconf} write /org/gnome/desktop/interface/cursor-theme "'catppuccin-latte-light-cursors'";
         ${getExe pkgs.dconf} write /org/gnome/desktop/interface/icon-theme "'Papirus-Light'";
         ${getExe pkgs.pywalfox-native} light
         ~/.config/specialisation/light/activate
     else
-        hyprctl setcursor "catppuccin-mocha-dark-cursors" 37
+        ${hyprctl} setcursor "catppuccin-mocha-dark-cursors" 37
         ${getExe pkgs.dconf} write /org/gnome/desktop/interface/cursor-theme "'catppuccin-mocha-dark-cursors'";
         ${getExe pkgs.dconf} write /org/gnome/desktop/interface/icon-theme "'Papirus-Dark'";
         ${getExe pkgs.pywalfox-native} dark
