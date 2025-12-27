c5c75ea51407cbe578c5ac6a6380c48d96a53cbb
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2025-12-28 04:41:58 +0900

    chore: add copy alias

--- home/desktop/scripts/tasks.nix
+++ home/desktop/scripts/tasks.nix
@@ -1,37 +1,42 @@
 {
+  inputs,
+  lib,
   pkgs,
   config,
   osConfig,
   ...
 }:
+let
+  inherit (lib.meta) getExe;
+  dms = getExe inputs.dank-material-shell.packages.${pkgs.stdenv.hostPlatform.system}.dms-shell;
+in
 {
-
   home.packages = [
     (pkgs.writeShellScriptBin "update-nixos" ''
       if ! nix profile upgrade --all; then
-        notify-send -a "NixOS" -u "critical" -i "distributor-logo-nixos" "update-nixos" "update failed"
+        ${getExe pkgs.libnotify} -a "NixOS" -u "critical" -i "distributor-logo-nixos" "update-nixos" "update failed"
         exit 1
       fi
       if nix flake update --flake "${config.home.homeDirectory}/dots-nix"; then
-        notify-send -a "NixOS" -u "low" -i "distributor-logo-nixos" "update-nixos" "update completed"
+        ${getExe pkgs.libnotify} -a "NixOS" -u "low" -i "distributor-logo-nixos" "update-nixos" "update completed"
       else
-        notify-send -a "NixOS" -u "critical" -i "distributor-logo-nixos" "update-nixos" "update failed"
+        ${getExe pkgs.libnotify} -a "NixOS" -u "critical" -i "distributor-logo-nixos" "update-nixos" "update failed"
       fi
     '')
 
     (pkgs.writeShellScriptBin "gc-nixos" ''
       if nh clean all; then
-        notify-send -a "NixOS" -u "low" -i "distributor-logo-nixos" "gc-nixos" "garbage collection completed"
+        ${getExe pkgs.libnotify} -a "NixOS" -u "low" -i "distributor-logo-nixos" "gc-nixos" "garbage collection completed"
       else
-        notify-send -a "NixOS" -u "critical" -i "distributor-logo-nixos" "gc-nixos" "garbage collection failed"
+        ${getExe pkgs.libnotify} -a "NixOS" -u "critical" -i "distributor-logo-nixos" "gc-nixos" "garbage collection failed"
       fi
     '')
 
     (pkgs.writeShellScriptBin "rebuild-nixos" ''
       if nh os switch -H "${osConfig.networking.hostName}"; then
-        notify-send -a "NixOS" -u "low" -i "distributor-logo-nixos" "rebuild-nixos" "rebuild completed"
+        ${getExe pkgs.libnotify} -a "NixOS" -u "low" -i "distributor-logo-nixos" "rebuild-nixos" "rebuild completed"
       else
-        notify-send -a "NixOS" -u "critical" -i "distributor-logo-nixos" "rebuild-nixos" "rebuild failed"
+        ${getExe pkgs.libnotify} -a "NixOS" -u "critical" -i "distributor-logo-nixos" "rebuild-nixos" "rebuild failed"
       fi
     '')
 
@@ -42,6 +47,10 @@
     (pkgs.writeShellScriptBin "nvim-test" ''
       nvim-clean
       rsync -avz --copy-links --chmod=D2755,F744 "$HOME/dots-nix/home/applications/nvim" "$HOME/.config"
+    '')
+
+    (pkgs.writeShellScriptBin "wl-copy" ''
+      ${dms} cl copy
     '')
   ];
 }
