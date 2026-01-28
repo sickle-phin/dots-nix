422d96d23090e9a0cc1c6784b83149da3220d948
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2026-01-28 23:32:54 +0900

    chore: update flake.lock

--- hosts/pink/default.nix
+++ hosts/pink/default.nix
@@ -2,7 +2,7 @@
   imports = [
     ../../modules
     "${
-      builtins.fetchTarball {
+      fetchTarball {
         url = "https://github.com/nix-community/disko/archive/refs/tags/latest.tar.gz";
         sha256 = "03jz60kw0khm1lp72q65z8gq69bfrqqbj08kw0hbiav1qh3g7p08";
       }
