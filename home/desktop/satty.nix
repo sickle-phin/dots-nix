cbeeed46724c8fc7624f0687621abd090b319e84
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2026-02-09 23:43:15 +0900

    chore

--- home/desktop/satty.nix
+++ home/desktop/satty.nix
@@ -1,10 +1,21 @@
 {
+  inputs,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib.meta) getExe;
+  dms = getExe inputs.dank-material-shell.packages.${pkgs.stdenv.hostPlatform.system}.dms-shell;
+in
+{
   programs.satty = {
     enable = true;
     settings = {
       general = {
         resize.mode = "smart";
         floating-hack = true;
+        copy-command = "${dms} cl copy -t image/png";
       };
       font = {
         family = "Inter Variable";
