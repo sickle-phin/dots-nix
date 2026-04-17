3dc71d7ca717f72d3ecfa21b1d2a6ddc4acbcac4
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2026-04-18 05:47:33 +0900

    feat: add description for keybinds

--- home/desktop/hyprland/binds.nix
+++ home/desktop/hyprland/binds.nix
@@ -15,16 +15,11 @@ in
     source = [ "${config.xdg.configHome}/hypr/dms/binds.conf" ];
     "$mod" = "SUPER";
     bind = [
-      "$mod, RETURN, exec, ${getExe pkgs.ghostty} +new-window"
-      "$mod, B, exec, uwsm-app -- ${getExe config.programs.firefox.finalPackage}"
-      "SUPER_SHIFT, B, exec, uwsm-app -- ${getExe config.programs.google-chrome.finalPackage}"
       "$mod, F, togglefloating"
       "SUPER_SHIFT, F, fullscreen"
-      "$mod, O, exec, uwsm-app -- ocr eng"
-      "SUPER_SHIFT, O, exec, uwsm-app -- ocr jpn"
       "$mod, P, pseudo"
       "$mod, Q, killactive"
-      "SUPER_SHIFT, Q, exec, hyprctl kill"
+      "SUPER_SHIFT, Q, forcekillactive"
       "$mod, H, movefocus, l"
       "$mod, L, movefocus, r"
       "$mod, K, movefocus, u"
@@ -33,24 +28,6 @@ in
       "SUPER_SHIFT, L, movewindow, r"
       "SUPER_SHIFT, K, movewindow, u"
       "SUPER_SHIFT, J, movewindow, d"
-
-      "$mod, A, exec, ${dms} ipc call widget toggle polyglot"
-      "SUPER_SHIFT, C, exec, ${dms} color pick -a"
-      "$mod, D, exec, ${dms} ipc call spotlight toggle"
-      "SUPER_SHIFT, E, exec, ${dms} ipc call powermenu toggle"
-      "$mod, M, exec, ${dms} ipc call processlist toggle"
-      "SUPER_SHIFT, N, exec, ${dms} ipc call notepad toggle"
-      "$mod, N, exec, ${dms} ipc call notifications toggle"
-      "$mod, T, exec, ${dms} ipc call theme toggle"
-      "$mod, V, exec, ${dms} ipc call clipboard toggle"
-      "$mod, W, exec, ${dms} ipc call dankdash wallpaper"
-      "$mod, comma, exec, ${dms} ipc call settings toggle"
-      "$mod, TAB, exec, ${dms} ipc call hypr toggleOverview"
-      "SUPER_SHIFT, S, exec, ${dms} screenshot --no-file"
-      ", Print, exec, ${dms} screenshot full --stdout | ${getExe pkgs.satty} -f -"
-      "Shift, Print, exec, ${dms} screenshot window --stdout | ${getExe pkgs.satty} -f -"
-      "$mod, Print, exec, pkill wl-screenrec || uwsm-app -- ${getExe pkgs.wl-screenrec} -f \"${config.home.homeDirectory}/Videos/Screencasts/$(date +%Y%m%d_%H%M%S).mp4\""
-
       "$mod, mouse_down, workspace, e+1"
       "$mod, mouse_up, workspace, e-1"
     ]
@@ -85,28 +62,53 @@ in
       "$mod SHIFT, equal, resizeactive, 0 10%"
     ];
 
-    bindel = [
-      ", XF86AudioRaiseVolume, exec, ${dms} ipc call audio increment 5"
-      ", XF86AudioLowerVolume, exec, ${dms} ipc call audio decrement 5"
-      "$mod, F6, exec, ${dms} ipc call audio increment 5"
-      "$mod, F5, exec, ${dms} ipc call audio decrement 5"
-      ", XF86MonBrightnessUp, exec, ${dms} ipc call brightness increment 5 \"\""
-      ", XF86MonBrightnessDown, exec, ${dms} ipc call brightness decrement 5 \"\""
+    bindd = [
+      "$mod, RETURN, Terminal, exec, ${getExe pkgs.ghostty} +new-window"
+      "$mod, B, Zen Browser, exec, uwsm-app -- ${getExe config.programs.firefox.finalPackage}"
+      "SUPER_SHIFT, B, Google Chrome, exec, uwsm-app -- ${getExe config.programs.google-chrome.finalPackage}"
+      "$mod, O, OCR(English), exec, uwsm-app -- ocr eng"
+      "SUPER_SHIFT, O, OCR(Japanese), exec, uwsm-app -- ocr jpn"
+      "$mod, A, Translation, exec, ${dms} ipc call widget toggle polyglot"
+      "SUPER_SHIFT, C, Color Picker, exec, ${dms} color pick -a"
+      "$mod, D, Launcher, exec, ${dms} ipc call spotlight toggle"
+      "SUPER_SHIFT, E, Power Menu, exec, ${dms} ipc call powermenu toggle"
+      "$mod, M, Process List, exec, ${dms} ipc call processlist toggle"
+      "$mod, N, Notifications, exec, ${dms} ipc call notifications toggle"
+      "SUPER_SHIFT, N, Notepad, exec, ${dms} ipc call notepad toggle"
+      "$mod, T, Toggle Dark/Light Theme, exec, ${dms} ipc call theme toggle"
+      "$mod, V, Clipboard History, exec, ${dms} ipc call clipboard toggle"
+      "$mod, W, Wallpapers, exec, ${dms} ipc call dankdash wallpaper"
+      "$mod, Comma, DMS Settings, exec, ${dms} ipc call settings toggle"
+      "$mod, Slash, Hyprland Keybinds, exec, ${dms} ipc call keybinds toggle hyprland"
+      "$mod, Tab, Overview, exec, ${dms} ipc call hypr toggleOverview"
+      "SUPER_SHIFT, S, Screenshot(Region), exec, ${dms} screenshot --no-file"
+      ", Print, Screenshot(Full), exec, ${dms} screenshot full --stdout | ${getExe pkgs.satty} -f -"
+      "Shift, Print, Screenshot(Focused Window), exec, ${dms} screenshot window --stdout | ${getExe pkgs.satty} -f -"
+      "$mod, Print, Screencast, exec, pkill wl-screenrec || uwsm-app -- ${getExe pkgs.wl-screenrec} -f \"${config.home.homeDirectory}/Videos/Screencasts/$(date +%Y%m%d_%H%M%S).mp4\""
     ];
 
-    bindl = [
-      ", XF86AudioMute, exec, ${dms} ipc call audio mute"
-      "$mod, F4, exec, exec, ${dms} ipc call audio mute"
-      ", XF86AudioMicMute, exec, ${dms} ipc call audio micmute"
-      ", XF86AudioPause, exec, ${dms} ipc call mpris playPause"
-      ", XF86AudioPlay, exec, ${dms} ipc call mpris playPause"
-      ", XF86AudioPrev, exec, ${getExe pkgs.playerctl} position -5"
-      ", XF86AudioNext, exec, ${getExe pkgs.playerctl} position +5"
+    binddel = [
+      ", XF86AudioRaiseVolume, Audio Increment, exec, ${dms} ipc call audio increment 5"
+      ", XF86AudioLowerVolume, Audio Decrement, exec, ${dms} ipc call audio decrement 5"
+      "$mod, F6, Audio Increment, exec, ${dms} ipc call audio increment 5"
+      "$mod, F5, Audio Decrement, exec, ${dms} ipc call audio decrement 5"
+      ", XF86MonBrightnessUp, Brightness Increment, exec, ${dms} ipc call brightness increment 5 \"\""
+      ", XF86MonBrightnessDown, Brightness Decrement, exec, ${dms} ipc call brightness decrement 5 \"\""
     ];
 
-    bindlo = [
-      ", XF86AudioPrev, exec, ${dms} ipc call mpris previous"
-      ", XF86AudioNext, exec, ${dms} ipc call mpris next"
+    binddl = [
+      ", XF86AudioMute, Audio Mute, exec, ${dms} ipc call audio mute"
+      "$mod, F4, Audio Mute, exec, ${dms} ipc call audio mute"
+      ", XF86AudioMicMute, Mic Mute, exec, ${dms} ipc call audio micmute"
+      ", XF86AudioPause, Media(Play/Pause), exec, ${dms} ipc call mpris playPause"
+      ", XF86AudioPlay, Media(Play/Pause), exec, ${dms} ipc call mpris playPause"
+      ", XF86AudioPrev, Media(Position -5sec), exec, ${getExe pkgs.playerctl} position -5"
+      ", XF86AudioNext, Media(Position +5sec), exec, ${getExe pkgs.playerctl} position +5"
+    ];
+
+    binddlo = [
+      ", XF86AudioPrev, Media(Skip to the Previous Track), exec, ${dms} ipc call mpris previous"
+      ", XF86AudioNext, Media(Skip to the Next Track), exec, ${dms} ipc call mpris next"
     ];
 
     bindm = [
