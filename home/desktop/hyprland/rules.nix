3287f3bdde668dbd13b244e962ca3a0135a2ca02
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2026-07-24 17:51:57 +0900

    chore: temp

--- home/desktop/hyprland/rules.nix
+++ home/desktop/hyprland/rules.nix
@@ -140,16 +140,16 @@ in
         type = "screencopy";
         mode = "allow";
       }
-      {
-        binary = "${pkgs.hyprlandPlugins.hypr-dynamic-cursors}/lib/libhypr-dynamic-cursors.so";
-        type = "plugin";
-        mode = "allow";
-      }
-      {
-        binary = "${pkgs.hyprlandPlugins.hyprfocus}/lib/libhyprfocus.so";
-        type = "plugin";
-        mode = "allow";
-      }
+      # {
+      #   binary = "${pkgs.hyprlandPlugins.hypr-dynamic-cursors}/lib/libhypr-dynamic-cursors.so";
+      #   type = "plugin";
+      #   mode = "allow";
+      # }
+      # {
+      #   binary = "${pkgs.hyprlandPlugins.hyprfocus}/lib/libhyprfocus.so";
+      #   type = "plugin";
+      #   mode = "allow";
+      # }
       {
         binary = ".*";
         type = "plugin";
