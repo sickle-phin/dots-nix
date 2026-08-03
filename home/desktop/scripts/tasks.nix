930fecd9a212619531cc0f9bd6153e6d918b22e8
sickle-phin <114330858+sickle-phin@users.noreply.github.com>
2026-08-03 09:39:08 +0900

    feat: add command

--- home/desktop/scripts/tasks.nix
+++ home/desktop/scripts/tasks.nix
@@ -10,46 +10,53 @@ let
   dms = getExe config.programs.dank-material-shell.package;
 in
 {
-  home.packages = [
-    (pkgs.writeShellScriptBin "update-nixos" ''
-      if ! nix profile upgrade --all; then
-        ${getExe pkgs.libnotify} -a "NixOS" -u "critical" -i "distributor-logo-nixos" "update-nixos" "update failed"
-        exit 1
-      fi
-      if nix flake update --flake "${config.home.homeDirectory}/dots-nix"; then
-        ${getExe pkgs.libnotify} -a "NixOS" -u "low" -i "distributor-logo-nixos" "update-nixos" "update completed"
-      else
-        ${getExe pkgs.libnotify} -a "NixOS" -u "critical" -i "distributor-logo-nixos" "update-nixos" "update failed"
-      fi
-    '')
+  home.packages =
+    let
+      rebuild =
+        command:
+        (pkgs.writeShellScriptBin "${command}-nixos" ''
+          profile=$(dms ipc powerprofile status)
+          cleanup() {
+            ${dms} ipc powerprofile set "$profile" &> /dev/null
+            ${dms} ipc inhibit disable &> /dev/null
+          }
+          trap cleanup EXIT
+          ${dms} ipc inhibit enable &> /dev/null
+          ${dms} ipc powerprofile set performance &> /dev/null
+          nh os ${command} -H "${osConfig.networking.hostName}"
+        '');
+    in
+    [
+      (pkgs.writeShellScriptBin "update-nixos" ''
+        if ! nix profile upgrade --all; then
+          ${getExe pkgs.libnotify} -a "NixOS" -u "critical" -i "distributor-logo-nixos" "update-nixos" "update failed"
+          exit 1
+        fi
+        if nix flake update --flake "${config.home.homeDirectory}/dots-nix"; then
+          ${getExe pkgs.libnotify} -a "NixOS" -u "low" -i "distributor-logo-nixos" "update-nixos" "update completed"
+        else
+          ${getExe pkgs.libnotify} -a "NixOS" -u "critical" -i "distributor-logo-nixos" "update-nixos" "update failed"
+        fi
+      '')
 
-    (pkgs.writeShellScriptBin "gc-nixos" ''
-      if nh clean all --no-gcroots; then
-        ${getExe pkgs.libnotify} -a "NixOS" -u "low" -i "distributor-logo-nixos" "gc-nixos" "garbage collection completed"
-      else
-        ${getExe pkgs.libnotify} -a "NixOS" -u "critical" -i "distributor-logo-nixos" "gc-nixos" "garbage collection failed"
-      fi
-    '')
+      "${rebuild "boot"}"
+      "${rebuild "switch"}"
 
-    (pkgs.writeShellScriptBin "rebuild-nixos" ''
-      profile=$(dms ipc powerprofile status)
-      cleanup() {
-        ${dms} ipc powerprofile set "$profile" &> /dev/null
-        ${dms} ipc inhibit disable &> /dev/null
-      }
-      trap cleanup EXIT
-      ${dms} ipc inhibit enable &> /dev/null
-      ${dms} ipc powerprofile set performance &> /dev/null
-      nh os switch -H "${osConfig.networking.hostName}"
-    '')
+      (pkgs.writeShellScriptBin "gc-nixos" ''
+        if nh clean all --no-gcroots; then
+          ${getExe pkgs.libnotify} -a "NixOS" -u "low" -i "distributor-logo-nixos" "gc-nixos" "garbage collection completed"
+        else
+          ${getExe pkgs.libnotify} -a "NixOS" -u "critical" -i "distributor-logo-nixos" "gc-nixos" "garbage collection failed"
+        fi
+      '')
 
-    (pkgs.writeShellScriptBin "nvim-clean" ''
-      rm -rf "$HOME/.config/nvim"
-    '')
+      (pkgs.writeShellScriptBin "nvim-clean" ''
+        rm -rf "$HOME/.config/nvim"
+      '')
 
-    (pkgs.writeShellScriptBin "nvim-test" ''
-      nvim-clean
-      rsync -avz --copy-links --chmod=D2755,F744 "$HOME/dots-nix/home/applications/nvim" "$HOME/.config"
-    '')
-  ];
+      (pkgs.writeShellScriptBin "nvim-test" ''
+        nvim-clean
+        rsync -avz --copy-links --chmod=D2755,F744 "$HOME/dots-nix/home/applications/nvim" "$HOME/.config"
+      '')
+    ];
 }
